642a4052ae01bf39d93d2178c1f604a4435807e7
Caroline Austin <caroline.austin@colorado.edu>
2023-04-11 18:41:28 -0600

    made more tolerant to number of responses and recoloring the bars, also decreased font size for the axis labels

--- MapStackedBarPlot.m
+++ MapStackedBarPlot.m
@@ -1,10 +1,10 @@
 function MapStackedBarPlot(All_map,Title,norm, legend, Color_list) %~ used to be legend
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-
+[row, col] = size(All_map);
 % Current_levels_str = ["0.1" ".5" "1" "1.5" "2" "2.5" "3" "3.5" "4"];
 b = bar(All_map/norm*100, "stacked"); % bi %need to calculate numsub differently
-for j = 1:4 % 4 should acutally be a variable that is part of the size of All_map
+for j = 1:col % 4 should acutally be a variable that is part of the size of All_map
     b(j).FaceColor = Color_list(j,:);
 end
 
@@ -14,6 +14,6 @@ end
 % % yticks([1 6]); 
 
 ax = gca;
-ax.FontSize = 27;
+ax.FontSize = 25; %27;
 title(Title, "FontSize", 45)
 end
