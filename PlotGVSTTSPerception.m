9c83f09fd1c34e6caa01a69a81f7a5d47fd4c34f
Caroline Austin <caroline.austin@colorado.edu>
2023-04-27 11:35:43 -0600

    added comments to code and fixed label

--- PlotGVSTTSPerception.m
+++ PlotGVSTTSPerception.m
@@ -10,19 +10,32 @@ function  PlotGVSTTSPerception(Shot_Label,GVS_Label, tilt,shot,GVS, time,colors,
     pos_legend(1) = "TTS Commanded Tilt";
     hold on;
     pos_length = length(prof2plot);
+    %plot each of the shot reports one at a time
     for i = 1:pos_length
+        %first figure out what the color will be
         color_index = 1;
         for j = 1:length(match_list)
+            %get the index (j) where the shot label matches one of the
+            %predefined current/prop combinations listed in match_list
             if contains(Shot_Label(prof2plot(i)), match_list(j))
+                % offset by one from the matching index because the 
+                % color list should include a default color as the first
+                % index
                 color_index = j+1;
                 continue;
             end
         end
         plot(time,shot(:,prof2plot(i)), colors(color_index))
-       
+        %save the label as part of the legend
         line_label = char(Shot_Label(prof2plot(i)));
         pos_legend(i+1) =(strrep(line_label(1:13), '_', '.'));
     end
+    %make the label intuitive to readers
+    pos_legend = strrep(pos_legend, '7.00', 'Velocity');
+    pos_legend = strrep(pos_legend, '7.50', 'Angle&Velocity');
+    pos_legend = strrep(pos_legend, '8.00', 'Angle');
+    pos_legend = strrep(pos_legend, '8.00', '');
+    legend(pos_legend)
     legend(pos_legend)
     xlabel('Time (s)');
     ylabel('Tilt (degrees)')
@@ -36,6 +49,8 @@ function  PlotGVSTTSPerception(Shot_Label,GVS_Label, tilt,shot,GVS, time,colors,
     ylabel('Angular Velocity (degrees/s)')
      xlim([0 35]);
 
+     %will want/need to change this code to match the other color selection
+     %code
     [gvs_prof] = find(contains(GVS_Label, 'command')); %'command'
 %     gvs_colors=['g'; 'g';'c'; 'c';'b';'b' ; colors];% PS1002
     gvs_colors=['g'; 'c'; 'b' ; colors];% PS1003 and 4
--- PlotGVSTTSPerceptionSHOTonly.m
+++ PlotGVSTTSPerceptionSHOTonly.m
@@ -7,19 +7,31 @@ function  PlotGVSTTSPerceptionSHOTonly(Shot_Label,GVS_Label, tilt,shot,GVS, time
     pos_legend(1) = "TTS Commanded Tilt";
     hold on;
     pos_length = length(prof2plot);
-    for i = 1:pos_length
+    %plot each of the shot reports one at a time
+    for i = 1:pos_length 
+        %first figure out what the color will be
         color_index = 1;
         for j = 1:length(match_list)
+            %get the index (j) where the shot label matches one of the
+            %predefined current/prop combinations listed in match_list
             if contains(Shot_Label(prof2plot(i)), match_list(j))
-                color_index = j+1;
+                % offset by one from the matching index because the 
+                % color list should include a default color as the first
+                % index
+                color_index = j+1; 
                 continue;
             end
         end
+        
         plot(time,shot(:,prof2plot(i)), colors(color_index))
-       
+       %save the label as part of the legend
         line_label = char(Shot_Label(prof2plot(i)));
         pos_legend(i+1) =(strrep(line_label(1:13), '_', '.'));
     end
+    pos_legend = strrep(pos_legend, '7.00', 'Velocity');
+    pos_legend = strrep(pos_legend, '7.50', 'Angle&Velocity');
+    pos_legend = strrep(pos_legend, '8.00', 'Angle');
+    pos_legend = strrep(pos_legend, '8.00', '');
     legend(pos_legend)
     xlabel('Time (s)');
     ylabel('Tilt (degrees)')
