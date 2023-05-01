9018926d17c29865545206dd288549d3f2a88605
Caroline Austin <caroline.austin@colorado.edu>
2023-05-01 16:22:00 -0600

    made able to easily change between adjusted/ non adjusted inputs/outputs and removed pos_colors variable

--- TTS_GVS_IndvPlot.m
+++ TTS_GVS_IndvPlot.m
@@ -4,7 +4,7 @@ clc;
 % code section 2 (run this 2nd - soon to be 3rd)
 Color_List = [ "black";"green";"cyan"; "blue";"red";"green"; "cyan";"blue"];
 match_list = ["N_4_00mA_7_00"; "N_4_00mA_7_50"; "N_4_00mA_8_00"; "0_00mA";"P_4_00mA_7_00"; "P_4_00mA_7_50"; "P_4_00mA_8_00"];
-
+datatype = '';
 %% 
 code_path = pwd; %save code directory
 file_path = uigetdir; %user selects file directory
@@ -25,9 +25,11 @@ for sub = 1:numsub
     end
 %     subject_path = [file_path, '\PS' , subject_str];
     subject_path = [file_path, '\' , subject_str];
+    subject_plot_path = [plots_path '\' subject_str];
+    mkdir(subject_plot_path);
 
     cd(subject_path);
-    load(['PS', subject_str, 'GroupAdj.mat ']);
+    load(['PS', subject_str, 'Group', datatype, '.mat ']);
 %     load(['PS', subject_str, '.mat ']);
     cd(code_path);
     [subject_filenames]=file_path_info2(code_path,subject_path ); % get files from file folder
@@ -37,16 +39,14 @@ for sub = 1:numsub
 
 %% Plot 4A with function
     [pos_prof] = find(contains(Label.shot_4A, 'P'));
-%     pos_colors=['r'; 'r'; 'g'; 'g';'c'; 'c';'b';'b']; % PS1002
-    pos_colors=Color_List;%['r'; 'r'; 'g'; 'c';'b']; % PS1003 and 4
     PlotGVSTTSPerception(Label.shot_4A,Label.GVS_4A, ...
         tilt_4A(1:trial_end,:),shot_4A(1:trial_end,:),GVS_4A(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,pos_prof,match_list);
+        time(1:trial_end),Color_List,pos_prof,match_list);
     subplot(3,1,1)
-    title('4A Positive GVS Affects on Tilt PerceptionAdj')
+    title(['4A Positive GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Positive_4A_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Positive_4A_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
@@ -55,27 +55,26 @@ for sub = 1:numsub
     neg_prof = [zero_prof ; neg_prof];
     PlotGVSTTSPerception(Label.shot_4A,Label.GVS_4A, ...
         tilt_4A(1:trial_end,:),shot_4A(1:trial_end,:),GVS_4A(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,neg_prof,match_list);
+        time(1:trial_end),Color_List,neg_prof,match_list);
     subplot(3,1,1)
-    title('4A Negative GVS Affects on Tilt PerceptionAdj')
+    title(['4A Negative GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Negative_4A_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Negative_4A_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
 
 %% Plot 4B with function
     [pos_prof] = find(contains(Label.shot_4B, 'P'));
-%     pos_colors=[ 'r'; 'g'; 'c';'b']; % PS1003 and 4
     PlotGVSTTSPerception(Label.shot_4B,Label.GVS_4B, ...
         tilt_4B(1:trial_end,:),shot_4B(1:trial_end,:),GVS_4B(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,pos_prof, match_list);
+        time(1:trial_end),Color_List,pos_prof, match_list);
     subplot(3,1,1)
-    title('4B Positive GVS Affects on Tilt PerceptionAdj')
+    title(['4B Positive GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Positive_4B_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Positive_4B_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
@@ -84,57 +83,52 @@ for sub = 1:numsub
     neg_prof = [zero_prof ; neg_prof];
     PlotGVSTTSPerception(Label.shot_4B,Label.GVS_4B, ...
         tilt_4B(1:trial_end,:),shot_4B(1:trial_end,:),GVS_4B(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,neg_prof,match_list);
+        time(1:trial_end),Color_List,neg_prof,match_list);
     subplot(3,1,1)
-    title('4B Negative GVS Affects on Tilt PerceptionAdj')
+    title(['4B Negative GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Negative_4B_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Negative_4B_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
 % Plot 5A with function
     [pos_prof] = find(contains(Label.shot_5A, 'P'));
-%     pos_colors=['r'; 'r'; 'g'; 'g'; 'g'; 'c'; 'c';'b';'b']; %PS1002
-%     pos_colors=['r'; 'r'; 'g'; 'c';'b']; % PS1003 and 4
     PlotGVSTTSPerception(Label.shot_5A,Label.GVS_5A, ...
         tilt_5A(1:trial_end,:),shot_5A(1:trial_end,:),GVS_5A(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,pos_prof,match_list);
+        time(1:trial_end),Color_List,pos_prof,match_list);
     subplot(3,1,1)
-    title('5A Positive GVS Affects on Tilt PerceptionAdj')
+    title(['5A Positive GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Positive_5A_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Positive_5A_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
-%     pos_colors=['r'; 'r'; 'g';  'g';'c'; 'c';'b';'b']; % PS1002
-%     pos_colors=['r'; 'r'; 'g'; 'c';'b']; % PS1003 and 4
     [neg_prof] = find(contains(Label.shot_5A, 'N'));
     [zero_prof] = find(contains(Label.shot_5A, '_0_'));
     neg_prof = [zero_prof ; neg_prof];
     PlotGVSTTSPerception(Label.shot_5A,Label.GVS_5A, ...
         tilt_5A(1:trial_end,:),shot_5A(1:trial_end,:),GVS_5A(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,neg_prof,match_list);
+        time(1:trial_end),Color_List,neg_prof,match_list);
     subplot(3,1,1)
-    title('5A Negative GVS Affects on Tilt PerceptionAdj')
+    title(['5A Negative GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Negative_5A_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Negative_5A_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
 
 %% Plot 5B with function
     [pos_prof] = find(contains(Label.shot_5B, 'P'));
-%     pos_colors=[ 'r'; 'g'; 'c';'b']; % PS1003 and 4
     PlotGVSTTSPerception(Label.shot_5B,Label.GVS_5B, ...
         tilt_5B(1:trial_end,:),shot_5B(1:trial_end,:),GVS_5B(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,pos_prof, match_list);
+        time(1:trial_end),Color_List,pos_prof, match_list);
     subplot(3,1,1)
-    title('5B Positive GVS Affects on Tilt PerceptionAdj')
+    title(['5B Positive GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Positive_5B_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Positive_5B_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
@@ -143,27 +137,25 @@ for sub = 1:numsub
     neg_prof = [zero_prof ; neg_prof];
     PlotGVSTTSPerception(Label.shot_5B,Label.GVS_5B, ...
         tilt_5B(1:trial_end,:),shot_5B(1:trial_end,:),GVS_5B(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,neg_prof,match_list);
+        time(1:trial_end),Color_List,neg_prof,match_list);
     subplot(3,1,1)
-    title('5B Negative GVS Affects on Tilt PerceptionAdj')
+    title(['5B Negative GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Negative_5B_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Negative_5B_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;    
 
 %% Plot 6A with function
     [pos_prof] = find(contains(Label.shot_6A, 'P'));
-%     pos_colors=['r'; 'r'; 'g'; 'c'; 'b']; %1003?
-%     pos_colors=['r'; 'r'; 'g'; 'c';'b']; % PS1003 and 4
     PlotGVSTTSPerception(Label.shot_6A,Label.GVS_6A, ...
         tilt_6A(1:trial_end,:),shot_6A(1:trial_end,:),GVS_6A(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,pos_prof,match_list);
+        time(1:trial_end),Color_List,pos_prof,match_list);
     subplot(3,1,1)
-    title('6A Positive GVS Affects on Tilt PerceptionAdj')
+    title(['6A Positive GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Positive_6A_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Positive_6A_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
@@ -172,27 +164,26 @@ for sub = 1:numsub
     neg_prof = [zero_prof ; neg_prof];
     PlotGVSTTSPerception(Label.shot_6A,Label.GVS_6A, ...
         tilt_6A(1:trial_end,:),shot_6A(1:trial_end,:),GVS_6A(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,neg_prof,match_list);
+        time(1:trial_end),Color_List,neg_prof,match_list);
     subplot(3,1,1)
-    title('6A Negative GVS Affects on Tilt PerceptionAdj')
+    title(['6A Negative GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Negative_6A_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Negative_6A_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
 
 %% Plot 6B with function
     [pos_prof] = find(contains(Label.shot_6B, 'P'));
-%     pos_colors=['r'; 'g'; 'c';'b']; % PS1003 and 4
     PlotGVSTTSPerception(Label.shot_6B,Label.GVS_6B, ...
         tilt_6B(1:trial_end,:),shot_6B(1:trial_end,:),GVS_6B(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,pos_prof,match_list);
+        time(1:trial_end),Color_List,pos_prof,match_list);
     subplot(3,1,1)
-    title('6B Positive GVS Affects on Tilt PerceptionAdj')
+    title(['6B Positive GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Positive_6B_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Positive_6B_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;
 
@@ -201,12 +192,12 @@ for sub = 1:numsub
     neg_prof = [zero_prof ; neg_prof];
     PlotGVSTTSPerception(Label.shot_6B,Label.GVS_6B, ...
         tilt_6B(1:trial_end,:),shot_6B(1:trial_end,:),GVS_6B(1:trial_end,:), ...
-        time(1:trial_end),pos_colors,neg_prof,match_list);
+        time(1:trial_end),Color_List,neg_prof,match_list);
     subplot(3,1,1)
-    title('6B Negative GVS Affects on Tilt PerceptionAdj')
+    title(['6B Negative GVS Affects on Tilt Perception' datatype])
 
-    cd(plots_path);
-    saveas(gcf, [ 'Negative_6B_4_00mA_PerceptionAdj' subject_str]); 
+    cd(subject_plot_path);
+    saveas(gcf, [ 'Negative_6B_4_00mA_Perception' datatype subject_str]); 
     cd(code_path);
     hold off;    
 
