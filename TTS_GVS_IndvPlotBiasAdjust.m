c0c2862b9a015660d6eb2e6ceff2cdd631f75269
Caroline Austin <caroline.austin@colorado.edu>
2023-05-18 14:13:32 -0600

    added comments, put bias calculation code into a function, added datatype suffix capability, implemented changes for the S instead of PS prefix

--- TTS_GVS_IndvPlotBiasAdjust.m
+++ TTS_GVS_IndvPlotBiasAdjust.m
@@ -1,19 +1,30 @@
+%% Script 4a for Dynamic GVS +Tilt
+% this script calculates and applies a L/R bias adjustment to the data
+% based on the average reported values for the sham trials compared to the
+% average acutal motion values for those same sham trials. A single 
+% bias value is calulated and applied for each subject.
+% it can read in data from script 2 or the other 4 scripts and can be used
+% in the 3, 4, and 5 scripts. 
 close all; 
 clear all; 
 clc; 
-%code section 2b
 
-%% 
+%% set up
+subnum = 1015:1015;  % Subject List 
+numsub = length(subnum);
+subskip = [1013 40005 40006];  %DNF'd subjects or subjects that didn't complete this part
+datatype = ''; %can change this to specify which data you want to use for the checkng
+% '' = regular , 'Time' = time adjusted, 'Adj' = Bias adjusted (can stack
+% multiple)
+
 code_path = pwd; %save code directory
 file_path = uigetdir; %user selects file directory
 plots_path = [file_path '\Plots']; % specify where plots are saved
 gvs_path = [file_path '\GVSProfiles'];
 [filenames]=file_path_info2(code_path, file_path); % get files from file folder
 
-subnum = 1014:1014;  % Subject List 
-numsub = length(subnum);
-subskip = [1013 40005 40006];  %DNF'd subjects or subjects that didn't complete this part
-
+%% calculate and apply bias adjustment
+%for each subject
 for sub = 1:numsub
     subject = subnum(sub);
     subject_str = num2str(subject);
@@ -23,93 +34,22 @@ for sub = 1:numsub
     end
     subject_path = [file_path, '\' , subject_str];
 %     subject_path = [file_path, '\PS' , subject_str];
-
+    %load in un-adjusted subject data
     cd(subject_path);
-    load(['PS', subject_str, 'Group.mat ']);
+    load(['S', subject_str, 'Group' datatype '.mat ']);
         
-    cd(code_path);
-    [subject_filenames]=file_path_info2(code_path,subject_path ); % get files from file folder
-    num_files = length(subject_filenames);
-    mat_filenames = get_filetype(subject_filenames, 'mat');
-    num_mat_files = length(mat_filenames);
-
-        tilt_4A_avg = mean(tilt_4A,"omitnan"); 
-        shot_4A_avg = mean(shot_4A, "omitnan"); 
-        index = 1;
-        for i = 1:length(tilt_4A_avg)
-            if ~ rem(i+2,3)
-            tilt_4A_actual_avg(:,index) = tilt_4A_avg(:,i);
-            index = index+1;
-            end
-        end
-        
-        bias_4A = tilt_4A_actual_avg-shot_4A_avg;
-
-
-        tilt_4B_avg = mean(tilt_4B,"omitnan"); 
-        shot_4B_avg = mean(shot_4B, "omitnan"); 
-        index = 1;
-        for i = 1:length(tilt_4B_avg)
-            if ~ rem(i+2,3)
-            tilt_4B_actual_avg(:,index) = tilt_4B_avg(:,i);
-            index = index+1;
-            end
-        end
-        
-        bias_4B = tilt_4B_actual_avg-shot_4B_avg;
-
-
-        tilt_5A_avg = mean(tilt_5A,"omitnan"); 
-        shot_5A_avg = mean(shot_5A, "omitnan"); 
-        index = 1;
-        for i = 1:length(tilt_5A_avg)
-            if ~ rem(i+2,3)
-            tilt_5A_actual_avg(:,index) = tilt_5A_avg(:,i);
-            index = index+1;
-            end
-        end
-        
-        bias_5A = tilt_5A_actual_avg-shot_5A_avg;
-
-
-        tilt_5B_avg = mean(tilt_5B,"omitnan"); 
-        shot_5B_avg = mean(shot_5B, "omitnan"); 
-        index = 1;
-        for i = 1:length(tilt_5B_avg)
-            if ~ rem(i+2,3)
-            tilt_5B_actual_avg(:,index) = tilt_5B_avg(:,i);
-            index = index+1;
-            end
-        end
-        
-        bias_5B = tilt_5B_actual_avg-shot_5B_avg;
-
-
-        tilt_6A_avg = mean(tilt_6A,"omitnan"); 
-        shot_6A_avg = mean(shot_6A, "omitnan"); 
-        index = 1;
-        for i = 1:length(tilt_6A_avg)
-            if ~ rem(i+2,3)
-            tilt_6A_actual_avg(:,index) = tilt_6A_avg(:,i);
-            index = index+1;
-            end
-        end
-        
-        bias_6A = tilt_6A_actual_avg-shot_6A_avg;
-
-
-        tilt_6B_avg = mean(tilt_6B,"omitnan"); 
-        shot_6B_avg = mean(shot_6B, "omitnan"); 
-        index = 1;
-        for i = 1:length(tilt_6B_avg)
-            if ~ rem(i+2,3)
-            tilt_6B_actual_avg(:,index) = tilt_6B_avg(:,i);
-            index = index+1;
-            end
-        end
-        
-        bias_6B = tilt_6B_actual_avg-shot_6B_avg;
-        
+    %calculate bias for all trials
+    bias_4A=calc_bias(tilt_4A, shot_4A);
+    bias_4B=calc_bias(tilt_4B, shot_4B);
+    bias_5A=calc_bias(tilt_5A, shot_5A);
+    bias_5B=calc_bias(tilt_5B, shot_5B);
+    bias_6A=calc_bias(tilt_6A, shot_6A);
+    bias_6B=calc_bias(tilt_6B, shot_6B);
+         
+        %find the indexes for the sham trials and then use the indices to
+        %to add together all of the biases across all sham trials,
+        %calculate the number of sham trials and divide the sum by the
+        %number of trials to get an average bias value. 
         bias_correction = 0;
         sham_indices_4A = find(contains(Label.shot_4A, '0_00mA'));
         for j = 1:length(sham_indices_4A)
@@ -141,6 +81,7 @@ for sub = 1:numsub
 
         bias_correction = bias_correction/ num_sham_trials;
 
+        %apply the bias correction to all trials
         shot_4A = shot_4A + bias_correction;
         shot_4B = shot_4B + bias_correction;
         shot_5A = shot_5A + bias_correction;
@@ -154,7 +95,7 @@ for sub = 1:numsub
    vars_2_save = ['Label Trial_Info time trial_end shot_4A tilt_4A GVS_4A  ' ...
        ' shot_5A tilt_5A GVS_5A shot_6A tilt_6A GVS_6A shot_4B tilt_4B GVS_4B  ' ...
        'shot_5B tilt_5B GVS_5B shot_6B tilt_6B GVS_6B'];
-   eval(['  save ' ['PS', subject_str, 'GroupAdj.mat '] vars_2_save ' vars_2_save']);      
+   eval(['  save ' ['S', subject_str, 'Group' datatype 'Bias.mat '] vars_2_save ' vars_2_save']);      
    cd(code_path)
    eval (['clear ' vars_2_save])
    close all;
@@ -162,11 +103,18 @@ for sub = 1:numsub
 
 end
 
-% %% debugging
-% for i = 1:16
-% %     if i == 17
-% %         plot(time, tilt_4A(:,i)); legend();
-% %     end
-% plot(time, tilt_4A(:,i)); legend();
-% hold on;
-% end
+function bias=calc_bias(motion, report)
+        %find average for each signal
+        motion_avg = mean(motion,"omitnan"); 
+        report_avg = mean(report, "omitnan"); 
+        index = 1;
+        %only need the average of the TTS actual 
+        for i = 1:length(motion_avg)
+            if ~ rem(i+2,3)
+            actual_avg(:,index) = motion_avg(:,i);
+            index = index+1;
+            end
+        end
+        % calculate the difference between averages to generate bias
+        bias = actual_avg-report_avg;
+end
