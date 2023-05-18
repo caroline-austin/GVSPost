ea8a70368ec77130aae9cdc39a62fa32b280e521
Caroline Austin <caroline.austin@colorado.edu>
2023-05-18 12:50:19 -0600

    cleaned up code and comments, changed PS prefix to S prefix

--- TTS_GVS_IndvGroupByProfile.m
+++ TTS_GVS_IndvGroupByProfile.m
@@ -1,20 +1,52 @@
+%% Script 2 for Dynamic GVS +Tilt
+% this script takes the individual matlab files generated in the previous
+% script and combines the data into a single matlab file with variables
+% made up of multiple trials(each trial is a separate column), but grouped
+% by the physical motion profile that the trial is associated with. The
+% trial each column is associated with is stored in the Label structure.
+% The results of this file are unadjusted, but are able to be used as inputs 
+% in any of the 3,4,5 scripts
 close all; 
 clear all; 
 clc; 
-% code section 2
 
-%% 
+%% set up
+subnum = 1015:1015;  % Subject List 
+numsub = length(subnum);
+subskip = [1013 40005 40006];  %DNF'd subjects or subjects that didn't complete this part
+
 code_path = pwd; %save code directory
 file_path = uigetdir; %user selects file directory (Data)
 plots_path = [file_path '\Plots']; % specify where plots are saved
 [filenames]=file_path_info2(code_path, file_path); % get file names from file folder
+   
+% combine individual trials for each subject
+for sub = 1:numsub
+    subject = subnum(sub);
+    subject_str = num2str(subject);
+    % skip subjects that DNF'd or there is no data for
+    if ismember(subject,subskip) == 1
+       continue
+    end
+    %directory with individual subject files (this naming convention is
+    %predefined)
+%     subject_path = [file_path, '\PS' , subject_str];
+    subject_path = [file_path, '\' , subject_str];
 
-subnum = 1011:1011;  % Subject List 
-numsub = length(subnum);
-subskip = [40005 40006];  %DNF'd subjects or subjects that didn't complete this part
+    % load overall file(has info about all trials from the "master"
+    % excel document)
+    cd(subject_path);
+    load(['PS', subject_str, '.mat ']);
+    cd(code_path);
+
+    %get info about indiviual trial files
+    [subject_filenames]=file_path_info2(code_path,subject_path ); % get files from file folder
+    num_files = length(subject_filenames);
+    mat_filenames = get_filetype(subject_filenames, 'mat');
+    num_mat_files = length(mat_filenames);
 
    %initialize/allocate the variables we plan to consolidate the trial
-   %info into
+   %variables into
    GVS_4A = [];
    tilt_4A = [];
    shot_4A = [];
@@ -33,45 +65,23 @@ subskip = [40005 40006];  %DNF'd subjects or subjects that didn't complete this 
    GVS_6B = [];
    tilt_6B = [];
    shot_6B = [];
-       
-%iterate through all subjects
-for sub = 1:numsub
-    subject = subnum(sub);
-    subject_str = num2str(subject);
-    % skip subjects that DNF'd or there is no data for
-    if ismember(subject,subskip) == 1
-       continue
-    end
-    %directory with individual subject files (this naming convention is
-    %predefined
-%     subject_path = [file_path, '\PS' , subject_str];
-    subject_path = [file_path, '\' , subject_str];
-
-    %load overall file(has info about all trials from the excel document)
-    cd(subject_path);
-    load(['PS', subject_str, '.mat ']);
-    cd(code_path);
-
-    %get info about indiviual trial files
-    [subject_filenames]=file_path_info2(code_path,subject_path ); % get files from file folder
-    num_files = length(subject_filenames);
-    mat_filenames = get_filetype(subject_filenames, 'mat');
-    num_mat_files = length(mat_filenames);
-
-    %iterate through all trial files
+   
+    %iterate through all trial files to store them into the grouped
+    %variables
     for file = 1:num_mat_files
         current_file = char(mat_filenames(file));
+       %skip files that aren't the trial files 
        if length(current_file)<26
            continue
        end
 
-       %get info and load trial file
+       %get info about and load trial file
        check_profile_name = current_file(8:9); %reduce by one when transitioning to regular subjects
        trial_number = current_file(end-5:end-4);
 %        current_val = current_file();
 %        proportional = current_file();
        cd(subject_path);
-       load([current_file]);
+       load(current_file);
        cd(code_path); 
 
         %store trial files based on the motion profile. the code should
@@ -161,22 +171,18 @@ for sub = 1:numsub
                     current_file);
                 [shot_6B, Label] = AddOnData1(shot_6B,shot_actual,  ... 
                     Label, 'shot_6B', 'shot_actual', current_file);
-
         end
-    
-
-
     end
-
+    %set up time vector for plotting later on
     time = (time- time(1))/1000;  
-
-
 
 %% save files
    cd(subject_path);
    vars_2_save = ['Label Trial_Info time trial_end shot_4A tilt_4A GVS_4A  ' ...
        ' shot_5A tilt_5A GVS_5A shot_6A tilt_6A GVS_6A shot_4B tilt_4B GVS_4B  ' ...
        'shot_5B tilt_5B GVS_5B shot_6B tilt_6B GVS_6B'];
+   %change filename for saving -> remove PS and possibly swap out "Group"
+   %for Extract or Sort
    eval(['  save ' ['PS', subject_str, 'Group.mat '] vars_2_save ' vars_2_save']);      
    cd(code_path)
    eval (['clear ' vars_2_save])
@@ -213,11 +219,3 @@ function [out_var, Label] = AddOnData1(out_var,data1, Label, out_var_name, ...
         "current_file(11:end-4)  name1]);"]));
 
 end
-% %% debugging
-% for i = 1:16
-% %     if i == 17
-% %         plot(time, tilt_4A(:,i)); legend();
-% %     end
-% plot(time, tilt_4A(:,i)); legend();
-% hold on;
-% end
