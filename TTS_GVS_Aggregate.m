b7311baf7750bbe682e025cf777579ed91aca234
Caroline Austin <caroline.austin@colorado.edu>
2023-05-18 14:42:28 -0600

    added comments and generally cleaned up code, also made it able to handle S instead of PS

--- TTS_GVS_Aggregate.m
+++ TTS_GVS_Aggregate.m
@@ -1,23 +1,31 @@
+%% Script 6 for Dynamic GVS +Tilt
+% this script averages together the data from all specified subjects, if a
+% trial does not exist for a subject it is not averaged in. 
+% the script can take input from scripts 2 and 4 and output should be used
+% for scripts 7 
+
 close all; 
 clear all; 
 clc; 
-% code section 6?
-
+%% set up
+subnum = 1015:1015;  % Subject List 
+numsub = length(subnum);
+subskip = [1006 1007 1008 1009 1010 1013 40006];  %DNF'd subjects or subjects that didn't complete this part
 match_list = ["N_4_00mA_7_00"; "N_4_00mA_7_50"; "N_4_00mA_8_00"; "0_00mA";"P_4_00mA_7_00"; "P_4_00mA_7_50"; "P_4_00mA_8_00"];
-datatype = 'Adj';
-%% 
+datatype = 'BiasTime';
+
 code_path = pwd; %save code directory
 file_path = uigetdir; %user selects file directory
 plots_path = [file_path '\Plots']; % specify where plots are saved
 gvs_path = [file_path '\GVSProfiles'];
 [filenames]=file_path_info2(code_path, file_path); % get files from file folder
 
-subnum = 1002:1005;  % Subject List 
-numsub = length(subnum);
-subskip = [1006 1007 1008 1009 1010 40006];  %DNF'd subjects or subjects that didn't complete this part
-
-tot_num = 0;
-All_shot_4A = zeros(1527,length(match_list)); %1527 is the length of the time series 
+%initalize all shot variables 
+% tot_num = 0;
+All_shot_4A = zeros(1527,length(match_list)); %1527 is the length of the non-adjusted time series 
+if contains(datatype, 'Time')
+    All_shot_4A = zeros(1427,length(match_list)); %1427 is the length of the time-adjusted time series
+end
 All_shot_4B = All_shot_4A;
 All_shot_5A = All_shot_4A;
 All_shot_5B = All_shot_4A;
@@ -30,8 +38,8 @@ num_trials_5B = num_trials_4A;
 num_trials_6A = num_trials_4A;
 num_trials_6B = num_trials_4A;
 
-% I want to average all of the data series together
-
+%add in the data for each subject while keeping track of how many trials
+%are averaged in
 for sub = 1:numsub
     subject = subnum(sub);
     subject_str = num2str(subject);
@@ -39,15 +47,19 @@ for sub = 1:numsub
     if ismember(subject,subskip) == 1
        continue
     end
-    tot_num = tot_num+1;   
+    %keep track of the number of subjects that get used 
+%     tot_num = tot_num+1;   
 
-    subject_path = [file_path, '\PS' , subject_str];
-
+    %load subject file
+    subject_path = [file_path, '\' , subject_str];
+%     subject_path = [file_path, '\PS' , subject_str];
     cd(subject_path);
-    load(['PS', subject_str, 'Group' datatype '.mat ']);
-    
+    load(['S', subject_str, 'Group' datatype '.mat ']);
     cd(code_path);
 
+    % add the current subject's data into the aggregate variable
+    %and increment the number of trials averaged into each trial type based on
+    %the trial data available for the current subject
     for i = 1:length(Label.shot_4A)
         for j = 1:length(match_list)
             if contains(Label.shot_4A(i), match_list(j))
@@ -98,6 +110,8 @@ for sub = 1:numsub
     end
 
 end
+%divide the aggregate report by the number of trials added into it to get
+%the average report across subjects (need to add a calculation of error)
 All_shot_4A = All_shot_4A./num_trials_4A;
 All_shot_4B = All_shot_4B./num_trials_4B;
 All_shot_5A = All_shot_5A./num_trials_5A;
@@ -105,6 +119,7 @@ All_shot_5B = All_shot_5B./num_trials_5B;
 All_shot_6A = All_shot_6A./num_trials_6A;
 All_shot_6B = All_shot_6B./num_trials_6B;
 
+%update the label
 Label.shot_4A = match_list;
 Label.shot_4B = match_list;
 Label.shot_5A = match_list;
