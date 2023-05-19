848b381e7a00f5052bc84da77eb05b499d5bc138
Caroline Austin <caroline.austin@colorado.edu>
2023-05-18 23:21:22 -0600

    commented and cleaned up code and updated it to handle S prefix, and handle less than or more than standard number of trials

--- TTS_GVS_Measures_Indv.m
+++ TTS_GVS_Measures_Indv.m
@@ -1,26 +1,20 @@
+%% Script 5b for Dynamic GVS +Tilt
+% this script calculates outcome measures (rms, total deflection ...) and
+% then plots these outcomes for all trial types to help better visualize
+% the data it takes its input from scripts 2 and 4 and should include
 close all; 
 clear all; 
 clc; 
-% code section 4a for TTS dynamic tilt + GVS stuff
-% (you don't need to run 3 for this one to work)
-
-%% initialize 
-blue = [ 0.2118    0.5255    0.6275];
-green = [0.5059    0.7451    0.6314];
-navy = [0.2196    0.2118    0.3804];
-purple = [0.4196    0.3059    0.4431];
-red =[0.7373  0.1529    0.1922];
-yellow = [255 190 50]/255;
-black =  [0 0 0];
-Color_list = [blue; green; yellow; red; black; navy; purple; ];
+%% set up
+subnum = 1015:1015;  % Subject List 
+numsub = length(subnum);
+subskip = [40005 40006];  %DNF'd subjects or subjects that didn't complete this part
+datatype = 'BiasTime';
 
 Color_List = [ "black";"green";"cyan"; "blue";"red";"green"; "cyan";"blue"];
 match_list = ["N_4_00mA_7_00"; "N_4_00mA_7_50"; "N_4_00mA_8_00"; "0_00mA";"P_4_00mA_7_00"; "P_4_00mA_7_50"; "P_4_00mA_8_00"];
 
-Color_listA = ['g'; 'c'; 'b'; 'r'; 'r'; 'g'; 'c'; 'b' ];
-Color_listB = ['g'; 'c'; 'b'; 'r'; 'r';  'g'; 'c'; 'b'];
-datatype = '';
-%% set up pathing
+% set up pathing
 code_path = pwd; %save code directory
 file_path = uigetdir; %user selects file directory
 plots_path = [file_path '\Plots\Measures']; % specify where plots are saved
@@ -28,11 +22,7 @@ gvs_path = [file_path '\GVSProfiles'];
 tts_path = [file_path '\TTSProfiles'];
 [filenames]=file_path_info2(code_path, file_path); % get files from file folder
 
-subnum = 1011:1011;  % Subject List 
-numsub = length(subnum);
-subskip = [40005 40006];  %DNF'd subjects or subjects that didn't complete this part
-
-tot_num = 0;
+%% calculate and plot the different measures for each subject
 for sub = 1:numsub
     subject = subnum(sub);
     subject_str = num2str(subject);
@@ -40,25 +30,16 @@ for sub = 1:numsub
     if ismember(subject,subskip) == 1
        continue
     end
-    tot_num = tot_num+1;   
-
+   
 %     subject_path = [file_path, '\PS' , subject_str];
     subject_path = [file_path, '\' , subject_str];
 
     cd(subject_path);
-    load(['PS', subject_str, 'Group', datatype '.mat ']);
+    load(['S', subject_str, 'Group', datatype '.mat ']);
     cd(code_path);
    
 
-%% RMS calculation and plots
-    %calculate and plot rms normalized by the 0mA response
-    % rms4A = rms(shot_4A(:,end-3)-shot_4A);
-    % rms4B = rms(shot_4B(:,end-3)-shot_4B);
-    % rms5A = rms(shot_5A(:,end-3)-shot_5A);
-    % rms5B = rms(shot_5B(:,end-3)-shot_5B);
-    % rms6A = rms(shot_6A(:,end-3)-shot_6A);
-    % rms6B = rms(shot_6B(:,end-3)-shot_6B);
-    
+% RMS calculation and plots   
     rms4A = rms(shot_4A);
     rms4B = rms(shot_4B);
     rms5A = rms(shot_5A);
@@ -66,30 +47,30 @@ for sub = 1:numsub
     rms6A = rms(shot_6A);
     rms6B = rms(shot_6B);
     
-    rmsA = [ rms4A; rms5A; rms6A ];
-    rmsB = [ rms4B; rms5B; rms6B ];
+%     rmsA = [ rms4A; rms5A; rms6A ];
+%     rmsB = [ rms4B; rms5B; rms6B ];
     
-    %%
+    % plot rms
     figure;
-    subplot(2,1,1)
-    A=bar([1, 2, 3 ], rmsA');
-    title ("A Profiles");
-    xticklabels(["SumOfSin4", "SumOfSin5", "SumOfSin6"]);
-    legend (["-4mA Vel", "-4 Ang & Vel", "-4 Ang", "0mA",  "0mA","+4mA Vel", "+4 Ang & Vel", "+4 Ang"], 'Location','north');
-    for j = 1:length(rmsA)
-        A(j).FaceColor = Color_listA(j,:);
-    end
-    
+    subplot(2,3,1)
+    plot_single_outcomes(rms4A,Label.shot_4A, Color_List,match_list);
+    title ("Profile 4A");
+    subplot(2,3,2)
+    plot_single_outcomes(rms5A,Label.shot_5A, Color_List,match_list);
+    title ("Profile 5A");
+    subplot(2,3,3)
+    plot_single_outcomes(rms6A,Label.shot_6A, Color_List,match_list);
+    title ("Profile 6A");
+    subplot(2,3,4)
+    plot_single_outcomes(rms4B,Label.shot_4B, Color_List,match_list);
+    title ("Profile 4B");
+    subplot(2,3,5)
+    plot_single_outcomes(rms5B,Label.shot_5B, Color_List,match_list);
+    title ("Profile 5B");
+    subplot(2,3,6)
+    plot_single_outcomes(rms6B,Label.shot_6B, Color_List,match_list);
+    title ("Profile 6B");
     hold on; 
-    
-    subplot(2,1,2)
-    B=bar([1, 2, 3], rmsB'); 
-    xticklabels(["SumOfSin4", "SumOfSin5", "SumOfSin6"]);
-    title ("B Profiles");
-    legend (["-4mA Vel", "-4 Ang & Vel", "-4 Ang", "0mA",  "+4mA Vel", "+4 Ang & Vel", "+4 Ang"],'Location','north');
-    for j = 1:length(rmsB)
-        B(j).FaceColor = Color_listB(j,:);
-    end
     sgtitle(['RMS: Subject ' datatype subject_str]);
 
     cd(plots_path);
@@ -97,15 +78,7 @@ for sub = 1:numsub
     cd(code_path);
     hold off;  
     
-    %% variance calculations and plots
-    %calculate and plot variance normalized by the 0mA response
-    % var4A = var(shot_4A(:,end-3)-shot_4A);
-    % var4B = var(shot_4B(:,end-3)-shot_4B);
-    % var5A = var(shot_5A(:,end-3)-shot_5A);
-    % var5B = var(shot_5B(:,end-3)-shot_5B);
-    % var6A = var(shot_6A(:,end-3)-shot_6A);
-    % var6B = var(shot_6B(:,end-3)-shot_6B);
-    
+    %% variance calculations and plots  
     var4A = var(shot_4A);
     var4B = var(shot_4B);
     var5A = var(shot_5A);
@@ -113,34 +86,69 @@ for sub = 1:numsub
     var6A = var(shot_6A);
     var6B = var(shot_6B);
     
-    varA = [ var4A; var5A; var6A ];
-    varB = [ var4B; var5B; var6B ];
+%     varA = [ var4A; var5A; var6A ];
+%     varB = [ var4B; var5B; var6B ];
     
     %%
     figure;
-    subplot(2,1,1)
-    A=bar([1, 2, 3], varA'); 
-    title ("A Profiles");
-    xticklabels(["SumOfSin4", "SumOfSin5", "SumOfSin6"]);
-    legend ([ "-4mA Vel", "-4 Ang & Vel", "-4 Ang", "0mA",  "0mA","+4mA Vel", "+4 Ang & Vel", "+4 Ang"], 'Location','north');
-    for j = 1:length(varA)
-        A(j).FaceColor = Color_listA(j,:);
-    end
-    
+    subplot(2,3,1)
+    plot_single_outcomes( var4A,Label.shot_4A, Color_List,match_list);
+    title ("Profile 4A");
+    subplot(2,3,2)
+    plot_single_outcomes( var5A,Label.shot_5A, Color_List,match_list);
+    title ("Profile 5A");
+    subplot(2,3,3)
+    plot_single_outcomes( var6A,Label.shot_6A, Color_List,match_list);
+    title ("Profile 6A");
+    subplot(2,3,4)
+    plot_single_outcomes( var4B,Label.shot_4B, Color_List,match_list);
+    title ("Profile 4B");
+    subplot(2,3,5)
+    plot_single_outcomes( var5B,Label.shot_5B, Color_List,match_list);
+    title ("Profile 5B");
+    subplot(2,3,6)
+    plot_single_outcomes( var6B,Label.shot_6B, Color_List,match_list);
+    title ("Profile 6B");
     hold on; 
-    
-    subplot(2,1,2)
-    B=bar([1, 2, 3], varB');%(:,2:end)
-    xticklabels(["SumOfSin4", "SumOfSin5", "SumOfSin6"]);
-    title ("B Profiles");
-    legend ([ "-4mA Vel", "-4 Ang & Vel", "-4 Ang", "0mA",  "+4mA Vel", "+4 Ang & Vel", "+4 Ang"],'Location','north');
-    for j = 1:length(varB)
-        B(j).FaceColor = Color_listB(j,:);
-    end
     sgtitle(['Variance: Subject ' datatype subject_str]);
 
     cd(plots_path);
     saveas(gcf, [ 'Variance' datatype subject_str  ]); 
     cd(code_path);
     hold off;  
+end
+
+
+function plot_single_outcomes(outcome,label, Color_List,match_list)
+    %plot data 
+    A=bar([1], [outcome]');
+    %format data
+    xticklabels([""]);
+    for j = 1:length(outcome)
+        color_index = 1;
+        for k = 1:length(match_list)
+            %get the index (j) where the shot label matches one of the
+            %predefined current/prop combinations listed in match_list
+            if contains(label(j), match_list(k))
+                % offset by one from the matching index because the 
+                % color list should include a default color as the first
+                % index
+                color_index = k+1;
+                outcome_legend(j) = match_list(k);
+                break;
+            end
+        end
+        %set to approcpriate color
+        A(j).FaceColor = Color_List(color_index,:);
+    end
+    %modify legend text to help make it more readable/understandable
+    outcome_legend = strrep(outcome_legend, '_', '.');
+    outcome_legend = strrep(outcome_legend, '.7.00', ' Vel');
+    outcome_legend = strrep(outcome_legend, '.7.50', ' Ang&Vel');
+    outcome_legend = strrep(outcome_legend, '.8.00', ' Ang');
+    outcome_legend = strrep(outcome_legend, '.00', '');
+    outcome_legend = strrep(outcome_legend, 'P.', '+');
+    outcome_legend = strrep(outcome_legend, 'N.', '-');
+    legend(outcome_legend)
+    hold on; 
 end
