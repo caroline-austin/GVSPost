c0f439aa3b22102903a7dd08301c5978eca33d39
LucWillett <111764225+LucWillett04@users.noreply.github.com>
2023-09-21 12:52:39 -0600

    Update IMUmetricsXsens.m
    Less user inputs and more graphs

--- IMUmetricsXsens.m
+++ IMUmetricsXsens.m
@@ -1,45 +1,65 @@
-clc;clear;close all;
+close all;clear;clc;
+%% set up
+subnum = 1017:1021;  % Subject List 
+numsub = length(subnum);
+subskip = [1013 1015 40005 40006];  %DNF'd subjects or subjects that didn't complete this part
 
-sub_dir = uigetdir;
-[~,name] = fileparts(sub_dir); % Determine the name of the folder
-
-IMU_FileName = uigetfile; % Determine the name of the file
-
-IMU_File = [sub_dir,'\',IMU_FileName]; % Create a location for readtable
-
-IMU_data = readtable(IMU_File);
-
-% Initialize structure variables
-euler(height(IMU_data)).x = 0;
-euler(height(IMU_data)).y = 0;
-euler(height(IMU_data)).z = 0;
-accel(height(IMU_data)).x = 0;
-accel(height(IMU_data)).y = 0;
-accel(height(IMU_data)).z = 0;
-gyr(height(IMU_data)).x = 0;
-gyr(height(IMU_data)).y = 0;
-gyr(height(IMU_data)).z = 0;
-
-% Read data into the structure
-for i=1:height(IMU_data)
-    euler(i).x = table2array(IMU_data(i,3));
-    euler(i).y = table2array(IMU_data(i,4));
-    euler(i).z = table2array(IMU_data(i,5));
-    accel(i).x = table2array(IMU_data(i,6));
-    accel(i).y = table2array(IMU_data(i,7));
-    accel(i).z = table2array(IMU_data(i,8));
-    gyr(i).x = table2array(IMU_data(i,9));
-    gyr(i).y = table2array(IMU_data(i,10));
-    gyr(i).z = table2array(IMU_data(i,11));
+% set up pathing
+code_path = pwd; %save code directory
+file_path = uigetdir; %user selects file directory
+if ismac || isunix
+    plots_path = [file_path '/Plots/Measures/MeanRemovedRMS']; % specify where plots are saved
+    gvs_path = [file_path '/GVSProfiles'];
+    tts_path = [file_path '/TTSProfiles'];
+elseif ispc
+    plots_path = [file_path '\Plots\Measures\MeanRemovedRMS']; % specify where plots are saved
+    gvs_path = [file_path '\GVSProfiles'];
+    tts_path = [file_path '\TTSProfiles'];
 end
+[filenames]=file_path_info2(code_path, file_path); % get files from file folder
 
-time = 0:0.05:((height(IMU_data)/20)-0.05); % Create time steps
+for sub = 1:numsub
+    subject = subnum(sub);
+    subject_str = num2str(subject);
+    % skip subjects that DNF'd or there is no data for
+    if ismember(subject,subskip) == 1
+       continue
+    end
 
-var_input = input('input data type to plot (EX: euler.x) ','s'); % Select a data type for the plot
+    % subject_path = [file_path, '\PS' , subject_str];
+    if ismac || isunix
+        subject_path = [file_path, '/' , subject_str, '/IMU_data'];
+        cd(subject_path);
+        imu_list = dir(subject_path);
 
-var_cat = eval(strcat('[', var_input ,']')); % Create a variable name in brackets for the plot 
+        % iterates through trials in the IMU folder. 
+        for i= 3:length(imu_list) 
+            imu_table = readtable(imu_list(i).name);
+            imu_data = table2array(imu_table(:,3:11));
+            time = 0:0.05:((height(imu_data)/20)-0.05);
+
+            plot(time, imu_data(:,1));
+
+        end
+  
+    elseif ispc
+        subject_path = [file_path, '\' , subject_str , '\IMU_data'];
+        cd(subject_path);
+        imu_list = dir(subject_path);
+
+        for i=length(imu_list)
+            imu_table = readtable(imu_list(i).name);
+            imu_data = table2array(imu_table(:,3:11));
+            time = 0:0.05:((height(imu_data)/20)-0.05);
+
+            plot(time, imu_data(:,1))
+            %saveas(gcf,'output','fig');
+        end
+    end
+    
+    cd(code_path);
+
+% all of the code you want to run 
 
 
-plot(time,var_cat);
-title(var_input);
-xlabel('time');
+end 
