cb5e10d79272f51186bfafd7b6df725e7f7892de
Caroline Austin <caroline.austin@colorado.edu>
2023-09-21 14:12:02 -0600

    data labeling

--- IMUmetricsXsens.m
+++ IMUmetricsXsens.m
@@ -35,14 +35,37 @@ for sub = 1:numsub
                
     end
 %     cd(subject_path);
+% change directories
+    cd(file_path);
+    Label.TrialInfo = readcell('DynamicGVSPlusTilt.xlsx','Sheet',['S' subject_str] ,'Range','P1:T1');
+    TrialInfo = readcell('DynamicGVSPlusTilt.xlsx','Sheet',['S' subject_str] ,'Range','P2:T13');
+    TrialInfo(cellfun(@(x) any(ismissing(x)), TrialInfo)) = {''};
+    cd(code_path);
+
     imu_list = file_path_info2(code_path, subject_path); % get files from file folder
     cd(subject_path);
     for i=length(imu_list)
+        if true % <change this to check if file type is not excel
+            continue
+        end
+        original_filename =imu_list(i);
         imu_table = readtable(string(imu_list(i)));
+        %pull label info from table and save into Label structure 
         imu_data = table2array(imu_table(:,3:11));
         time = 0:0.05:((height(imu_data)/20)-0.05);
         figure;
         plot(time, imu_data)
+
+
+        trial_name = horzcat([TrialInfo(i,2) '_' string(TrialInfo(i,3)) 'mA_' TrialInfo(i,4) '_' string(TrialInfo(i,4)) 'Hz']);
+
+            %% save files
+       cd(subject_path);
+       vars_2_save = ['Label original_filename imu_data time' ];
+       eval(['  save ' ['S' subject_str 'IMU' trial_name '.mat '] vars_2_save ' vars_2_save']);      
+       cd(code_path)
+       eval (['clear ' vars_2_save])
+       close all;
     end
             %saveas(gcf,'output','fig');
     cd(code_path);
