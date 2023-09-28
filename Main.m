98035b5f942b46a9842752c4c5e24753241a9879
aaronallred <88723734+aaronallred@users.noreply.github.com>
2023-09-28 11:21:19 -0600

    Updates

--- Main.m
+++ Main.m
@@ -1,19 +1,20 @@
 % Main Script
 clc;clear;close all;
+DataName = "DynamicData.mat";
 
 % Aggregate Data
 TTS_GVS_GainAdjust_Aaron
 TTS_GVS_Aggregate_Final
-save("DynamicData.mat") % Save Aggregate Data to .mat
+save(DataName) % Save Aggregate Data to .mat
+Var = load(DataName);
 
 % Plot Data
-PlotGroupPerceptions('Angle')
-PlotGroupPerceptions('Semi')
-PlotGroupPerceptions('Velocity')
-% 
-% PlotGroupGainScatter('Angle')
-% PlotGroupGainScatter('Semi')
-% PlotGroupGainScatter('Velocity')
+PlotGroupPerceptions('Angle',Var)
+PlotGroupPerceptions('Semi',Var)
+PlotGroupPerceptions('Velocity',Var)
+
+PlotIndPerceptions(Var)
+PlotNoGVSGroupPerceptions(Var)
 
 % Compute Metrics (store in .csv)
 MetricCompute_Gain("AngleGain")
