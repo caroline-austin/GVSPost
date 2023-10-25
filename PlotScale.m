77c4ab64383008ef6ae4c68386987fe8949ef128
LucWillett04 <luc.willett@colorado.edu>
2023-10-25 13:56:37 -0600

    GravityAligned adjustment for new IMU

--- PlotScale.m
+++ PlotScale.m
@@ -1,6 +1,6 @@
-function [Xlimit, Ylimit] = PlotScale(imu_data,time)
+function [Xlimit, Ylimit] = PlotScale(DataSet,time)
 %PlotScale scales the IMU data on both the x and y axis
 
-Ylimit = [max(max(imu_data(:,1:3))), max(max(imu_data(:,4:6))), max(max(imu_data(:,7:9)))];
-Xlimit = [time(1,length(time)-30*10) time(1,length(time)-30*2)];
+Ylimit = [min(min(DataSet)), max(max(DataSet))];
+Xlimit = [time(1,length(time)-30*10), time(1,length(time)-30*2)];
 end
