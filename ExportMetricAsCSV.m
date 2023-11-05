7f2c8f28336f1eaeba0391e5a577a4fa90b1b4b9
caroline-austin <caroline.austin@colorado.edu>
2023-11-04 23:37:44 -0600

    Second File Export
    Added code to allow for a specification of +/- netural GVS coupling and added code to create a second file that includes info on motion profile too

--- ExportMetricAsCSV.m
+++ ExportMetricAsCSV.m
@@ -4,19 +4,59 @@ code_path = pwd;
 [file_name,file_path] = uigetfile; %user selects file directory
 cd(file_path)
 load(file_name)
-
-var_name = "slope_save_all"; % for tilt-perception 
+cd(code_path)
+%%
+var_name = "peak_save_all"; % have to know aggregate variable name (check before hand)
 eval("var = " + var_name + ";");
 row_index = 0; 
-for i = 1:width(var)
-    for j = 1:length(var)
+for couple = 1:width(var)
+    for sub = 1:length(var)
         row_index = row_index+1;
-        var_anova(row_index,1) = j;
-        var_anova(row_index,2) = i;
-        var_anova(row_index,3) = var(j,i);
+        var_anova(row_index,1) = sub;
+        var_anova(row_index,2) = couple;
+        var_anova(row_index,3) = var(sub,couple);
+        if couple <4
+                var_anova(row_index, 4) = 'N'; 
+        elseif couple >4
+                var_anova(row_index, 4) = 'P'; 
+        else
+                var_anova(row_index, 4) = 'X';
+        end
     end
 end
-var_anova = array2table(var_anova,'VariableNames',{'SID', 'CouplingScheme', 'Var'});
+var_anova = array2table(var_anova,'VariableNames',{'SID', 'CouplingScheme', 'Var', 'CouplingDirection'});
 % eval("writetable(" + var_name + ", '" + file_name(1:end-4) + ".csv');");
+cd(file_path)
 eval("writetable(var_anova, '" + file_name(1:end-4) + ".csv');");
+cd(code_path)
+
+%%
+motion_list = ["4"; "4"; "5"; "5";"6"; "6"];
+direction_list = ["A"; "B"; "A"; "B";"A"; "B"];
+for motion = 1:length(motion_list)
+    eval("var = peak_save_" + motion_list(motion) + direction_list(motion) + ";"); % have to know aggregate variable name (check before hand)
+%     eval("var = " + var_name_full + ";");
+    row_index = 0; 
+    for couple = 1:width(var)
+        for sub = 1:length(var)
+            row_index = row_index+1;
+            var_anova_full(row_index,1) = sub;
+            var_anova_full(row_index,2) = couple;
+            var_anova_full(row_index,3) = var(sub,couple);
+            var_anova_full(row_index,5) = motion_list(motion);
+            var_anova_full(row_index,6) = direction_list(motion);
+            if couple <4
+                var_anova_full(row_index, 4) = 'N'; 
+            elseif couple >4
+                var_anova_full(row_index, 4) = 'P'; 
+            else
+                var_anova_full(row_index, 4) = 'X';
+            end
+        end
+    end
+end
+var_anova_full = array2table(var_anova_full,'VariableNames',{'SID', 'CouplingScheme', 'Var', 'CouplingDirection', 'MotionProfile', 'MotionDirection'});
+% eval("writetable(" + var_name + ", '" + file_name(1:end-4) + ".csv');");
+cd(file_path)
+eval("writetable(var_anova_full, '" + file_name(1:end-4) + "Full.csv');");
 cd(code_path)
