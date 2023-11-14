ef0935b6406a7b4e0a7cd1c09a0f96ff668d097f
NickBoggess <92459269+NickBoggess@users.noreply.github.com>
2023-11-14 09:14:51 -0700

    more updates

--- IMUmetricsXsens.m
+++ IMUmetricsXsens.m
@@ -1,5 +1,5 @@
 close all;clear;clc;
-
+tic
 %% set up
 subnum = 1017:1021;  % Subject List 
 numsub = length(subnum);
@@ -93,6 +93,19 @@ for sub = 1:numsub % first for loop that iterates through subject files
         t = (0:L-1)*T; % time vector 
         time_verify = time - t; % should result in zeros vector
 
+                %x_vec = FS/L*(0:L-1);
+        x_vec = FS/L*(-L/2:L/2-1);
+        fft_med = median(abs(fourier_calc));
+        [Max,ind] = max(abs(fourier_calc(2:length(gyro_aligned))));
+        mnxfft_freq(i) = x_vec(ind);
+        
+        
+        figure();
+        stem(x_vec,abs(fftshift(fourier_calc))); hold on;
+        %plot(x_vec,abs(smoothdata(fourier_calc,"gaussian",5))); hold off;
+        title('Complex Magnitude of FFT');
+        xlabel('Frequency [Hz]'); ylabel("|fft|");
+
 %         figure();
 %         sgtitle(trial_name)
 %         for j=1:width(imu_data) % nested for loop that plots each column inside of an IMU file 
@@ -181,7 +194,7 @@ for sub = 1:numsub % first for loop that iterates through subject files
     %eval (['clear ' vars_2_save])
     file_count = 0;
 end    
-
+toc
 % all of the code you want to run 
 
 
