9bba3e1768713b09bf942bc8c4f0da144b180c5e
caroline-austin <caroline.austin@colorado.edu>
2023-12-17 16:05:19 -0700

    Update analyzeFMT plots
    worked to make the first plot have everything that we want the rest of the plots to have

--- Data Vis/analyzeFMT.m
+++ Data Vis/analyzeFMT.m
@@ -15,6 +15,22 @@ function [data_compiled] = analyzeFMT(fileName,individual,plotB)
 %%%%%%%%%%%%
 %% Individual Participant
 
+%% Plotting set up
+% colors- first 5 are color blind friendly colors
+blue = [ 0.2118    0.5255    0.6275];
+green = [0.5059    0.7451    0.6314];
+navy = [0.2196    0.2118    0.3804];
+purple = [0.4196    0.3059    0.4431];
+red =[0.7373  0.1529    0.1922];
+yellow = [255 190 50]/255;
+Color_list = [blue; green; yellow; red; navy; purple];
+% sub_symbols = [">-k"; "v-k";"o-k";"+-k"; "*-k"; "x-k"; "square-k"; "^-k"; "<-k"; "pentagram-k"; "diamond-k"];
+sub_symbols = [">k"; "vk";"ok";"+k"; "*k"; "xk"; "squarek"; "^k"; "<k"; "pentagramk"; "diamondk"];
+yoffset = [0.1;0.1;0.1;0.1;0.1;-0.1;-0.1;-0.1;-0.1;-0.1;0]; 
+yoffset2 = [0.05; -0.05;0.05;-0.05;0.05;-0.05]; 
+xoffset1 = [-100;-80;-60;-40;-20;0;20;40;60;80;100]; 
+xoffset2 = [-0.25;-0.2;-0.15;-0.1;-0.05;0;0.05;0.1;0.15;0.2;0.25]; 
+
 %% Read File Generation
 load(fileName);
 if individual ~= 0
@@ -109,17 +125,59 @@ if individual == 0
     %%%Plotting
     if plotB == 1
 
-        %%% Pure Dasa Visualization
-        figure(); hold on;
+        %%% Pure Data Visualization
+        % define figure
+        fig = figure(); hold on;
+        % used tiledlayout so that we can adjust the margin setting
+        t=tiledlayout(2,1,'TileSpacing','tight');
         sgtitle('FMT Perfomance Data')
-        subplot(2,1,1)
-        scatter(GVS_admin, raw_time_data);
-        xlabel('GVS Gain Value');
-        ylabel('Raw Time of Course Completion (s)')
-        subplot(2,1,2)
-        scatter(GVS_admin, error_data);
-        xlabel('GVS Gain Value');
-        ylabel('Amount of Errors');
+        
+        %top tile is raw time data
+        nexttile
+        % string together 1st and 2nd iterations of condition into single
+        % column
+        raw_time_plot = [raw_time(:, [1 3 5]); raw_time(:, [2 4 6]) ];
+        %make box plot
+        boxchart(raw_time_plot)
+        hold on;
+        % plot individual subject data, divide by 500 so that values are 0,
+        % 1 and 2 then add 1 so that it is 1, 2, 3 and lines up with
+        % boxplots
+        % use x offset to separate subject symbols from each other
+        for i = 1:11
+            plot(GVS_admin((i*6)-5: (i*6))/500+ 1+ xoffset2(i), raw_time_data((i*6)-5: (i*6)),sub_symbols(i),'MarkerSize',15);
+            hold on;
+        end
+        % no labels for the top plot to save space
+        set(gca,'Xticklabel',[])
+        % xlabel('GVS Gain');
+        % y axis settings
+        yticks([5 10 15 20 25])
+        ylim([10 28])
+        ylabel('Raw Time (s)')
+        grid on;
+        % lower plot is the errors 
+        nexttile
+        % string together 1st and 2nd iterations of condition into single
+        % column for the box plot
+        errors_plot = [errors(:, [1 3 5]); errors(:, [2 4 6]) ];
+        boxchart(errors_plot)
+        hold on;
+        % plot indv subj data using with same xoffset calcs as above and y
+        % offset since both values for a subj could be the same 
+        for i = 1:11
+            plot(GVS_admin((i*6)-5: (i*6))/500 +1+ xoffset2(i), error_data((i*6)-5: (i*6))+yoffset2,sub_symbols(i),'MarkerSize',15);
+            hold on;
+        end
+        % add x and y labels
+        xticklabels( ["0"; "500"; "999"]);
+        xlabel('GVS Gain');
+        ylim([-0.5 5.5])
+        ylabel('Errors');
+        %set font size for the figure so it's legible
+        fontsize(fig, 32, "points")
+        
+        
         
 
         %%% Learning Effect over time
