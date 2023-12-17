da757892d84a292b5016e23fe45e4d2fb11d32e4
Tim-Behrer <147452551+Tim-Behrer@users.noreply.github.com>
2023-12-16 18:19:38 -0700

    Ran all ANOVAs for all three test conditions

--- Data Vis/main.m
+++ Data Vis/main.m
@@ -41,6 +41,10 @@ FMT_Data_tbl.Subj = categorical(FMT_Data_tbl.Subj);
 % random effects (so our within subjects) 
 lme = fitlme(FMT_Data_tbl,'RawTime ~ GVS + Order + (1|Subj)','FitMethod','REML','DummyVarCoding','effects'); % can also add GVS*Order, but the effect is not significant, so should probably exclude
 FMT_RT_AN = anova(lme) % left the ; off so that it prints the results to the command window
+lme = fitlme(FMT_Data_tbl,'Errors ~ GVS + Order + (1|Subj)','FitMethod','REML','DummyVarCoding','effects'); % can also add GVS*Order, but the effect is not significant, so should probably exclude
+FMT_ER_AN = anova(lme) % left the ; off so that it prints the results to the command window
+lme = fitlme(FMT_Data_tbl,'CorrectedTime ~ GVS + Order + (1|Subj)','FitMethod','REML','DummyVarCoding','effects'); % can also add GVS*Order, but the effect is not significant, so should probably exclude
+FMT_NT_AN = anova(lme) % left the ; off so that it prints the results to the command window
 
 % %this was me checking single effects models but they are set up pretty
 % %much the same way
@@ -69,12 +73,50 @@ FMT_RT_AN = anova(lme) % left the ; off so that it prints the results to the com
 % anovan(FMT_Data(:,1),{num2str(FMT_Data(:,5)),subj},'model',1,'random',2,'varnames',{'Order','Subj'});
 % anovan(FMT_Data(:,1),{(FMT_Data(:,5)),(FMT_Data(:,4)),subj},'model','interaction','continuous',[1],'random',3,'varnames',{'Order','GVS','Subj'});
 
-anova_RT = anovan(FMT_Data(:,1),{num2str(FMT_Data(:,4)),num2str(FMT_Data(:,5))},'model','interaction','varnames',{'GVS Admin','Trial Order'});
-anova_ER = anovan(FMT_Data(:,2),{num2str(FMT_Data(:,4)),num2str(FMT_Data(:,5))},'model','interaction','varnames',{'GVS Admin','Trial Order'});
-anova_NT = anovan(FMT_Data(:,3),{num2str(FMT_Data(:,4)),num2str(FMT_Data(:,5))},'model','interaction','varnames',{'GVS Admin','Trial Order'});
+% anova_RT = anovan(FMT_Data(:,1),{num2str(FMT_Data(:,4)),num2str(FMT_Data(:,5))},'model','interaction','varnames',{'GVS Admin','Trial Order'});
+% anova_ER = anovan(FMT_Data(:,2),{num2str(FMT_Data(:,4)),num2str(FMT_Data(:,5))},'model','interaction','varnames',{'GVS Admin','Trial Order'});
+% anova_NT = anovan(FMT_Data(:,3),{num2str(FMT_Data(:,4)),num2str(FMT_Data(:,5))},'model','interaction','varnames',{'GVS Admin','Trial Order'});
 %% Romberg
-Rom_Data = analyzeRomberg('ExcelData_Cut_ALL.mat',1);
-FMT_Table = table(Rom_Data(:,1),Rom_Data(:,2),num2str(Rom_Data(:,3)),num2str(Rom_Data(:,4)),...
-    'VariableNames',{'Failure Time','Head Tilt','GVS','Order'});
+Rom_Data = analyzeRomberg('ExcelData_Cut_ALL.mat',0);
+% first add a subject column to the data
+subj = cat(1, ones(24,1), ones(24,1)*2, ones(24,1)*3, ones(24,1)*4, ones(24,1)*5, ...
+    ones(24,1)*6, ones(24,1)*7, ones(24,1)*8, ones(24,1)*9, ones(24,1)*10, ones(24,1)*11);
+Rom_Data = [Rom_Data,subj];
+% then convert the data into an array
+ROM_Data_tbl = array2table(Rom_Data);
+ROM_Data_tbl.Properties.VariableNames = {'FailTime', 'headTilt', 'GVS', 'Order', 'Subj'};
+% make indep. var's categorical 
+ROM_Data_tbl.headTilt = categorical(ROM_Data_tbl.headTilt);
+ROM_Data_tbl.GVS = categorical(ROM_Data_tbl.GVS);
+ROM_Data_tbl.Order = categorical(ROM_Data_tbl.Order);
+ROM_Data_tbl.Subj = categorical(ROM_Data_tbl.Subj);
+% create linear mixed effect model with dependent ~ independent (1|Var) =
+% random effects (so our within subjects) 
+lme = fitlme(ROM_Data_tbl,'FailTime ~ headTilt + GVS*headTilt + Order + (1|Subj)','FitMethod','REML','DummyVarCoding','effects'); % can also add GVS*Order, but the effect is not significant, so should probably exclude
+ROM_FT_AN = anova(lme) % left the ; off so that it prints the results to the command window
 
-anova_FT = anovan(Rom_Data(:,1),{num2str(Rom_Data(:,2)),num2str(Rom_Data(:,3)),num2str(Rom_Data(:,4))},'model','interaction','varnames',{'Head Tilt','GVS Admin','Trial Order'});
+% anova_FT = anovan(Rom_Data(:,1),{num2str(Rom_Data(:,2)),num2str(Rom_Data(:,3)),num2str(Rom_Data(:,4))},'model','interaction','varnames',{'Head Tilt','GVS Admin','Trial Order'});
+
+
+%% Tandem
+Tan_Data = analyzeTandem('ExcelData_Cut_ALL.mat',0);
+% first add a subject column to the data
+subj = cat(1, ones(12,1), ones(12,1)*2, ones(12,1)*3, ones(12,1)*4, ones(12,1)*5, ...
+    ones(12,1)*6, ones(12,1)*7, ones(12,1)*8, ones(12,1)*9, ones(12,1)*10, ones(12,1)*11);
+Tan_Data = [Tan_Data,subj];
+% then convert the data into an array
+Tan_Data_tbl = array2table(Tan_Data);
+Tan_Data_tbl.Properties.VariableNames = {'testTime', 'goodSteps','eyesOpen','headTilt', 'GVS', 'Order', 'Subj'};
+% make indep. var's categorical 
+Tan_Data_tbl.eyesOpen = categorical(Tan_Data_tbl.eyesOpen);
+Tan_Data_tbl.headTilt = categorical(Tan_Data_tbl.headTilt);
+Tan_Data_tbl.GVS = categorical(Tan_Data_tbl.GVS);
+Tan_Data_tbl.Order = categorical(Tan_Data_tbl.Order);
+Tan_Data_tbl.Subj = categorical(Tan_Data_tbl.Subj);
+% create linear mixed effect model with dependent ~ independent (1|Var) =
+% random effects (so our within subjects) 
+lme = fitlme(Tan_Data_tbl,'testTime ~ headTilt + eyesOpen + GVS*headTilt*eyesOpen + Order + (1|Subj)','FitMethod','REML','DummyVarCoding','effects'); % can also add GVS*Order, but the effect is not significant, so should probably exclude
+Tan_TT_AN = anova(lme) % left the ; off so that it prints the results to the command window
+lme = fitlme(Tan_Data_tbl,'goodSteps ~ headTilt + eyesOpen + GVS*headTilt*eyesOpen + Order + (1|Subj)','FitMethod','REML','DummyVarCoding','effects'); % can also add GVS*Order, but the effect is not significant, so should probably exclude
+Tan_GS_AN = anova(lme) % left the ; off so that it prints the results to the command window
+
