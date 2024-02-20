e4b0dd6bd4c5220d5f465e5257a70ae37a207fb6
caroline-austin <caroline.austin@colorado.edu>
2024-02-19 22:15:00 -0700

    Update X2BExcelPlots.m
    updated to have boxplots

--- X2B/X2BExcelPlots.m
+++ X2B/X2BExcelPlots.m
@@ -16,9 +16,15 @@ plots_path = [file_path '/Plots']; % specify where plots are saved
 cd(code_path); cd .. ;
 [foldernames]=file_path_info2(code_path, file_path); % get foldernames from file folder
 
-subnum = 2034:2036;  % Subject List 
+subnum = 2034:2038;  % Subject List 
 numsub = length(subnum);
 subskip = [40005 40006];  %DNF'd subjects or subjects that didn't complete this part
+
+sub_symbols = ["kpentagram";"k<";"khexagram";"k>"; "kdiamond";"kv";"ko";"k+"; "k*"; "kx"; "ksquare"; "k^";];
+yoffset = [0.1;0.1;0.1;0.1;0.1;-0.1;-0.1;-0.1;-0.1;-0.1;0]; 
+yoffset2 = [0.05; -0.05;0.05;-0.05;0.05;-0.05]; 
+xoffset1 = [-100;-80;-60;-40;-20;0;20;40;60;80;100]; 
+xoffset2 = [-0.25;-0.2;-0.15; -0.15; -0.1;-0.05;0;0.05;0.1;0.15;0.2;0.25]; 
 
 %%
 total_results = zeros(3,2);
@@ -36,20 +42,26 @@ for sub = 1:numsub
     load(['S' subject_str '.mat']);
 
     main_results = cell2mat(main_results);
-    figure;
-    sgtitle(['S' subject_str ' Total Wins'])
-    subplot(2,1,1)
-    bar(main_results(:,1));
-    title ("Most Motion Sensation");
-    xticklabels([])
-    subplot(2,1,2)
-    bar(main_results(:,2));
-    xticklabels([Label.MainResultsRow])
-    title ("Most Tingling");
 
+    %% can add any indv. subject plots here
+    % figure;
+    % sgtitle(['S' subject_str ' Total Wins'])
+    % subplot(2,1,1)
+    % bar(main_results(:,1));
+    % title ("Most Motion Sensation");
+    % xticklabels([])
+    % subplot(2,1,2)
+    % bar(main_results(:,2));
+    % xticklabels([Label.MainResultsRow])
+    % title ("Most Tingling");
+
+    % aggregating results
     total_results = total_results +main_results;
+    sub_motion(:,sub) = main_results(:,1); 
+    sub_tingle(:,sub) = main_results(:,2); 
 end
 
+%% can add any aggregate subj plots here
 figure;
 sgtitle(['SAll Total Wins'])
 subplot(2,1,1)
@@ -58,6 +70,35 @@ title ("Most Motion Sensation");
 xticklabels([])
 subplot(2,1,2)
 bar(total_results(:,2));
+xticklabels([Label.MainResultsRow])
+title ("Most Tingling");
+
+%%
+figure;
+sgtitle(['SAll Total Wins'])
+subplot(2,1,1)
+b=boxplot(sub_motion');
+hold on;
+for j = 1:numsub
+    for i = 1:width(sub_motion')
+        
+        plot(i+xoffset2(j), sub_motion(i, j),sub_symbols(j),'MarkerSize',15,"LineWidth", 1.5);
+        hold on;
+    end
+end
+title ("Most Motion Sensation");
+xticklabels([])
+
+subplot(2,1,2)
+boxplot(sub_tingle');
+hold on;
+for j = 1:numsub
+    for i = 1:width(sub_motion')
+        
+        plot(i+xoffset2(j), sub_tingle(i, j),sub_symbols(j),'MarkerSize',15,"LineWidth", 1.5);
+        hold on;
+    end
+end
 xticklabels([Label.MainResultsRow])
 title ("Most Tingling");
 
