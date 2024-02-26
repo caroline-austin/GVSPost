698db83a426d50be11ff6dc1fd94a27b0923c364
caroline-austin <caroline.austin@colorado.edu>
2024-02-26 09:59:11 -0700

    Update scratch_plot_XSENS.m
    updated formatting for KGVS and correlation plot for IEEE presentation

--- scratch_plot_XSENS.m
+++ scratch_plot_XSENS.m
@@ -23,20 +23,22 @@ sub_symbols = ["ko";"k+"; "k*"; "kx"; "ksquare"; "k^";];
 fig = figure;
 plot(fft_SpHz_accy_sort(:,11),Gain_sub(5:10),'.','MarkerSize',0.001); hold on;
 h=lsline(); hold on;
+h.LineWidth = 4;
 line_y=h.YData;
 line_x=h.XData;
 corr_slope=(line_y(2)-line_y(1))/(line_x(2)-line_x(1));
 corr_slope_2 = corrcoef(fft_SpHz_accy_sort(:,11),Gain_sub(5:10));
 mdl = fitlm(fft_SpHz_accy_sort(:,11),Gain_sub(5:10));
 for i = 1:6
-    plot(fft_SpHz_accy_sort(i,11),Gain_sub(i+4),sub_symbols(i),'MarkerSize',15, LineWidth= 2);
+    plot(fft_SpHz_accy_sort(i,11),Gain_sub(i+4),sub_symbols(i),'MarkerSize',25, LineWidth= 2);
     hold on;
 end
-
+ylim([0.25 2])
 title('Medial-Lateral Sway Near 1Hz v. KGVS');
 xlabel('Amount of Sway Near 1Hz at 2mA (m/s^2)');
 ylabel('K_G_V_S ');
 fontsize(fig, 32, "points")
+fig.Position = [100 100 1000 500];
 
 %%
 
@@ -60,5 +62,6 @@ title('K_G_V_S GVS Effect');
 xlabel('');
 xticks([1])
 xticklabels(['']);
-ylabel('K_G_V_S ');
+% ylabel('K_G_V_S ');
 fontsize(fig, 32, "points")
+fig.Position = [100 100 400 500];
