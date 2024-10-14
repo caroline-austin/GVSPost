bf59945f03c2c1f48bc0ac119daa8729c9cc93bd
caroline-austin <caroline.austin@colorado.edu>
2024-10-14 14:02:56 -0600

    Update GVSPostX2A_IMU_Mbient.m
    Updated Buffer times based on raw IMU + time stamp info. added code to sort/organize imu data. added code to output the corrected gyroscope data in addition to the acc. commened out the metric calculations - added code to save the processed data in a .mat file

--- X2A/GVSPostX2A_IMU_Mbient.m
+++ X2A/GVSPostX2A_IMU_Mbient.m
@@ -8,29 +8,33 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc;clear;
 restoredefaultpath;
+code_path = pwd;
 
 %% Experimental Methods Specifications
-sub_dir = uigetdir; %user selects file directory './Subject Data/'; %I replaced this so the person can directly choose where to pull the data from
+file_path = uigetdir; %user selects file directory './Subject Data/'; %I replaced this so the person can directly choose where to pull the data from
 conditions = {'A'}; % right now ideally just one condition, might have an exp. A and B
 datatype = {'Accelerometer','Gyroscope'};
 
-numtrials = 57; % this number will vary, should not exceed 60
-subnum = [2001:2010];  % Subject List 
+numtrials = 30; % this number will vary, should not exceed 60
+subnum = [2006];  % Subject List 2001:2010 2001:2010
 groups = [1]; % ex: 1:Control 2:VisualCM % but I don't have groups yet so here all are the same
-subskip = [2001 2008 2010];  %DNF'd subjects
+subskip = [2001 2004 2008 2010];  %DNF'd subjects
 
 % additional start buffer for IMU data (sec.) row = cond.; col = subject
-buffer = [0;...
-          0;...
-          0]; 
+% this accounts for the time difference btween starting the imu and
+% starting the stop watch 
+buffer = [8 14 8 8  9 8 6 0 13 0];%...
+          % 8.5 9 0 0 0 0 0 0 0 0;...
+          % 8.5 9 0 0 0 0 0 0 0 0]; 
 
 % seconds to truncate off each trial [start, end]
 trunc = [0.3 -0.3]; %seconds 
 
-% Used to find buffer time for timed trial arrays
+% Used to find buffer time for timed trial arrays- this plot is not super
+% helpful for finding the buffers with the GVSPost data
 findbuffers = 1; % 1 is on other num is off
 % Used to view sensor orientation over time
-sensorpositionplot =1; % 1 is on other num is off
+sensorpositionplot =0; % 1 is on other num is off
 
 %% Initialize Storage Variables
 numsub = length(subnum);
@@ -53,6 +57,7 @@ p2pXa = rmsXYa; p2pYa = p2pXa;
 %% Loop Through and get Metrics defining Sway
 for sub = 1:numsub
     subject = subnum(sub);
+    subject_str = num2str(subject);
     % skip subjects that DNF'd
     if ismember(subject,subskip) == 1
        continue
@@ -60,15 +65,15 @@ for sub = 1:numsub
     for cond = 1:length(conditions)
 
         % Get File names
-        accelfile = [sub_dir,'/',num2str(subject),'/',...
+        accelfile = [file_path,'/',num2str(subject),'/',...
             num2str(subject),'-',conditions{cond},'-',...
             datatype{1},'.csv'];
 
-        gyrofile = [sub_dir,'/',num2str(subject),'/',...
+        gyrofile = [file_path,'/',num2str(subject),'/',...
             num2str(subject),'-',conditions{cond},'-',...
             datatype{2},'.csv'];
 
-        timestampfile = [sub_dir,'/',num2str(subject),'/',...
+        timestampfile = [file_path,'/',num2str(subject),'/',...
             num2str(subject),'-All-TimeStamps.xlsx'];
         
         if isfile(accelfile)==0
@@ -79,6 +84,8 @@ for sub = 1:numsub
         acceldata = readtable(accelfile,'ReadVariableNames',false);
         gyrodata = readtable(gyrofile,'ReadVariableNames',false);
         timestamps = readtable(timestampfile,'ReadVariableNames',false);
+
+        numtrials = ceil(height(timestamps)/2); 
   
         % Ensure data aligns across measurement types
         time = 1/100:1/100:max(acceldata.Var3); % create aligned time vec %adjust for sampling freq
@@ -89,109 +96,214 @@ for sub = 1:numsub
         gyro = pi/180*gyro;
 
         % Rotate accelerometer Data (x-ML y-AP z-EarthVertical)
-        acc_aligned = GravityAligned(acc, gyro,sensorpositionplot);
+        [acc_aligned, gyro_aligned] = GravityAligned(acc, gyro,sensorpositionplot);
+        %%
 
         % Get Trial Times
         Times = GetTrialTimes(timestamps,buffer(cond,sub),cond);
 
+
         % Make plot to find buffer
         if findbuffers == 1
-            figure(1);
+            figure();
             clf(1);
             hold on
-            plot(time,acc_aligned(:,3));
-            scatter(Times+repmat(trunc',numtrials,1),mean(acc_aligned(:,3))*...
-                ones(length(Times),1))
+            plot(time,gyro_aligned(:,1)); %acc_aligned(:,3)
+            scatter(Times+repmat(trunc',numtrials,1),mean(gyro(:,1))*...
+                ones(length(Times),1)); % acc_aligned(:,3)
             hold off
-            pause;
+            % pause;
         end
 
-        % Get Modified Romberg Balance data from IMU for each trial
-        start_time = buffer(cond,sub); 
-        for trial = 1:numtrials
 
-            s = Times(trial*2-1)+trunc(1); % trial start time
-            e = Times(trial*2)  +trunc(2); % trial end time
-                
-            % Get trial data
-            i1 = time>=s; i2 = time<=e;
-            trial_ind = i1.*i2;
-            trial_accel = acc_aligned(trial_ind==1,:);
-            trial_time = time(trial_ind==1);
+%% sort trials to save 
+trial_key = [file_path,'/',num2str(subject), '/A' , num2str(subject), '.mat'];
+load(trial_key);
+
+for trial =1:numtrials
+    if trial<31
+        Config =TrialInfo1(trial,4);
+        Current =TrialInfo1(trial,5);
+        Profile = strjoin(string(TrialInfo1(trial,7)));
+    else
+        Config= TrialInfo2(trial-30,4);
+        Current =TrialInfo2(trial-30,5);
+        Profile = strjoin(string(TrialInfo2(trial-30,7)));
+    end
+
+
+     %assign storage index value for each of our variables of
+            %interest that we want to index by
+            switch string(Config)
+                case 'Binaural'
+                    config_index = 1;
+                case 'Cevette'
+                    config_index = 2;
+                case 'Aoyama'
+                    config_index = 3;
+                otherwise
+                    config_index = 0; 
+            end
+            
+            switch string(Current)
+                case '0.1'
+                    current_index = 1;
+                case '0.5'
+                    current_index = 2;
+                case '1'
+                    current_index = 3;
+                case '1.5'
+                    current_index = 4;
+                case '2'
+                    current_index = 5;
+                case '2.5'
+                    current_index = 6;
+                case '3'
+                    current_index = 7;
+                case '3.5'
+                    current_index = 8;
+                case '4'
+                    current_index = 9;
+                otherwise
+                    current_index = 0;
+            end
     
-            % Store state information
-            accel(sub,trial,cond).x = trial_accel(:,1);
-            accel(sub,trial,cond).y = trial_accel(:,2);
-            accel(sub,trial,cond).z = trial_accel(:,3);
-            vel(sub,trial,cond).x = ...
-                cumtrapz(trial_time,trial_accel(:,1));
-            vel(sub,trial,cond).y = ...
-                cumtrapz(trial_time,trial_accel(:,2));
-            vel(sub,trial,cond).z = ...
-                cumtrapz(trial_time,trial_accel(:,3));
-            pos(sub,trial,cond).x = ...
-                cumtrapz(trial_time,cumtrapz(trial_time,trial_accel(:,1)));
-            pos(sub,trial,cond).y = ...
-                cumtrapz(trial_time,cumtrapz(trial_time,trial_accel(:,2)));
-            pos(sub,trial,cond).z = ...
-                cumtrapz(trial_time,cumtrapz(trial_time,trial_accel(:,3)));
-
-            % METRICS
-            rmsxy = rms([trial_accel(:,1), trial_accel(:,2)]);
-            p2pa = peak2peak(trial_accel);
-            %%% Acceleration metrics
-            % rms metrics
-%             rmsXYa(sub,trial,cond) = sqrt(1/2*(rmsxy(1)^2+rmsxy(2)^2));
-            rmsXYa(sub,trial,cond) = sqrt((rmsxy(1)^2+rmsxy(2)^2));
-            rmsXa(sub,trial,cond) = rmsxy(1);
-            rmsYa(sub,trial,cond) = rmsxy(2);
-            % peak-to-peak metrics
-            p2pXa(sub,trial,cond) = p2pa(1); 
-            p2pYa(sub,trial,cond) = p2pa(2); 
-
+           switch string(Profile)
+               case 'front'
+                    profile_index = 1;
+                case 'right'
+                    profile_index = 1;
+               case 'back'
+                    profile_index = 2;
+               case 'left'
+                    profile_index = 2;
+                case '0.25'
+                    profile_index = 3;
+                case '0.5'
+                    profile_index = 4;
+                case '1'
+                    profile_index = 5;
+                otherwise
+                    profile_index = 0;
+            end
+        %error checking
+        if current_index == 0 || profile_index == 0 || config_index == 0
+            continue
         end
+    imu_data{current_index,profile_index,config_index} = [acc_aligned(floor(Times(trial*2-1)*100):ceil(Times(trial*2)*100),1:3) gyro_aligned(floor(Times(trial*2-1)*100):ceil(Times(trial*2)*100),1:3)];
+    % imu_data{current_index,profile_index,config_index,:,4:6} = gyro(Times(trial*2-1):Times(trial*2),1:3);
+
+
+
+
+
+end
+    Label.IMUcell = ["AccX" "AccY" "AccZ" "Roll" "Pitch" "Yaw"];
+    Label.IMU = ["Current"; "Config";"Profile"];
+    Label.CurrentAmp = [0.1 , 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0];
+    Label.Profile = ["front/right" "back/left" "sin 0.25Hz" "sin 0.5Hz" "sin 1Hz"];
+    Label.Config = ["Binaural" "Cevete" "Aoyama"];
+
+
+    %% Save file
+    cd([file_path, '/' , subject_str]); %move to directory where file will be saved
+    %add all variables that we want to save to a list must include space
+    %between variable names 
+    vars_2_save =  ['Label TrialInfo1 TrialInfo2 imu_data acc_aligned gyro_aligned' ...
+        ' EndImpedance StartImpedance MaxCurrent MinCurrent ']; 
+    eval(['  save ' ['A', subject_str,'imu.mat '] vars_2_save ' vars_2_save']); %save file     
+    cd(code_path) %return to code directory
+    %clear saved variables to prevent them from affecting next subjects' data
+    eval (['clear ' vars_2_save]) 
+
+%%
+% %%
+%         % Get Modified Romberg Balance data from IMU for each trial
+%         start_time = buffer(cond,sub); 
+%         for trial = 1:numtrials
+% 
+%             s = Times(trial*2-1)+trunc(1); % trial start time
+%             e = Times(trial*2)  +trunc(2); % trial end time
+% 
+%             % Get trial data
+%             i1 = time>=s; i2 = time<=e;
+%             trial_ind = i1.*i2;
+%             trial_accel = acc_aligned(trial_ind==1,:);
+%             trial_time = time(trial_ind==1);
+% 
+%             % Store state information
+%             accel(sub,trial,cond).x = trial_accel(:,1);
+%             accel(sub,trial,cond).y = trial_accel(:,2);
+%             accel(sub,trial,cond).z = trial_accel(:,3);
+%             vel(sub,trial,cond).x = ...
+%                 cumtrapz(trial_time,trial_accel(:,1));
+%             vel(sub,trial,cond).y = ...
+%                 cumtrapz(trial_time,trial_accel(:,2));
+%             vel(sub,trial,cond).z = ...
+%                 cumtrapz(trial_time,trial_accel(:,3));
+%             pos(sub,trial,cond).x = ...
+%                 cumtrapz(trial_time,cumtrapz(trial_time,trial_accel(:,1)));
+%             pos(sub,trial,cond).y = ...
+%                 cumtrapz(trial_time,cumtrapz(trial_time,trial_accel(:,2)));
+%             pos(sub,trial,cond).z = ...
+%                 cumtrapz(trial_time,cumtrapz(trial_time,trial_accel(:,3)));
+% 
+%             % METRICS
+%             rmsxy = rms([trial_accel(:,1), trial_accel(:,2)]);
+%             p2pa = peak2peak(trial_accel);
+%             %%% Acceleration metrics
+%             % rms metrics
+% %             rmsXYa(sub,trial,cond) = sqrt(1/2*(rmsxy(1)^2+rmsxy(2)^2));
+%             rmsXYa(sub,trial,cond) = sqrt((rmsxy(1)^2+rmsxy(2)^2));
+%             rmsXa(sub,trial,cond) = rmsxy(1);
+%             rmsYa(sub,trial,cond) = rmsxy(2);
+%             % peak-to-peak metrics
+%             p2pXa(sub,trial,cond) = p2pa(1); 
+%             p2pYa(sub,trial,cond) = p2pa(2); 
+        % 
+        % end
     end
 end
 
-%% Choose Metric
-metrictype = 'rmsXYa';
-metricpick = 'median';
-
-switch metrictype
-    case 'rmsXYa'
-        ax = [];
-        ax1 = [];
-        metric = rmsXYa;
-        ylab = 'XY-plane rms of acceleration';
-    case 'p2pXa'
-        ax = [];
-        ax1 = [];
-        metric = p2pXa;
-        ylab = 'X-p2p acceleration';
-    case 'p2pYa'
-        ax = [];
-        ax1 = [];
-        metric = p2pYa;
-        ylab = 'Y-p2p acceleration';
-end
-
-switch metricpick
-    case 'median'
-        metric = median(metric(:,1:numtrials,:),2);
-        trial = 1;
-    case 'mean'
-        metric = mean(metric(:,1:numtrials,:),2);
-        trial = 1;
-    case 'min'
-        trial = 1;
-        for i = 1:numsub
-            for j = 1:length(conditions)
-                metric(i,trial,j) = min(metric(i,1:numtrials,j));
-            end
-        end
-    case 'trial'
-        trial = 2; %specify trial of interest
-end
+% %% Choose Metric
+% metrictype = 'rmsXYa';
+% metricpick = 'median';
+% 
+% switch metrictype
+%     case 'rmsXYa'
+%         ax = [];
+%         ax1 = [];
+%         metric = rmsXYa;
+%         ylab = 'XY-plane rms of acceleration';
+%     case 'p2pXa'
+%         ax = [];
+%         ax1 = [];
+%         metric = p2pXa;
+%         ylab = 'X-p2p acceleration';
+%     case 'p2pYa'
+%         ax = [];
+%         ax1 = [];
+%         metric = p2pYa;
+%         ylab = 'Y-p2p acceleration';
+% end
+% 
+% switch metricpick
+%     case 'median'
+%         metric = median(metric(:,1:numtrials,:),2);
+%         trial = 1;
+%     case 'mean'
+%         metric = mean(metric(:,1:numtrials,:),2);
+%         trial = 1;
+%     case 'min'
+%         trial = 1;
+%         for i = 1:numsub
+%             for j = 1:length(conditions)
+%                 metric(i,trial,j) = min(metric(i,1:numtrials,j));
+%             end
+%         end
+%     case 'trial'
+%         trial = 2; %specify trial of interest
+% end
 
 %% Do some stats
 % Thinking ANOVA (3 levels to our categorical IV / have to pick our DV)
@@ -309,7 +421,7 @@ end
 function Times = GetTrialTimes(timestamps,buffer,cond)
     
     if cond == 1
-        TimeStamps = timestamps.Var2;
+        TimeStamps = timestamps.Var3;
     elseif cond == 2
         TimeStamps = timestamps.Var5;
     elseif cond == 3
@@ -332,7 +444,7 @@ function Times = GetTrialTimes(timestamps,buffer,cond)
     end
 end
 
-function  acc_aligned = GravityAligned(acc, gyro,sensorpositionplot)
+function  [acc_aligned,gyro_aligned] = GravityAligned(acc, gyro,sensorpositionplot)
     FUSE = imufilter('SampleRate',25);
     q = FUSE(acc,gyro); % goes from Inertial to Sensor
     Eulers = eulerd(q, 'ZYX', 'frame'); % sensor = Rx'*Ry'*Rz'*global
@@ -352,6 +464,7 @@ function  acc_aligned = GravityAligned(acc, gyro,sensorpositionplot)
         % Rz = [cosd(yaw) -sind(phi) 0; 
         %       sind(yaw) cosd(yaw) 0; 0 0 1];
         acc_aligned(i,:) = (Ry*Rx*acc(i,:)')'; % Rz*Ry*Rx*sensor to go back
+        gyro_aligned(i,:) = (Ry*Rx*gyro(i,:)')'; 
     end
 
     if sensorpositionplot == 1
