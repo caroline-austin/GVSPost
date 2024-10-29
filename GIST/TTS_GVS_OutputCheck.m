13a578b69cac13e815283b9dfb0544fec100c2bb
caroline-austin <caroline.austin@colorado.edu>
2024-10-29 14:04:35 -0600

    Update TTS_GVS_OutputCheck.m
    changes to look at new data from 10/29/24

--- GIST/TTS_GVS_OutputCheck.m
+++ GIST/TTS_GVS_OutputCheck.m
@@ -30,7 +30,7 @@ cd(code_path)
 % filenames if they match then proceed with making comparison plots 
 for i = 1:num_csv_files   
     current_csv_file = char(csv_filenames(i));
-    csv_num = str2num(current_csv_file(11:12)); %19:20
+    csv_num = str2num(current_csv_file(20:21)); %19:20 11:12
     for j = 1:height(TrialKey)
         
         if csv_num == (TrialKey.TTSTrial(j))
@@ -38,7 +38,7 @@ for i = 1:num_csv_files
             Trial_name = strjoin(["Prof" TrialKey.TTSProfile(j) ", GVS" ...
                 TrialKey.MaxGVS(j) ", Angle" TrialKey.MaxAngle(j) ", Velocity" ...
                 TrialKey.MaxVelocity(j) ", Decay" TrialKey.UserDecay(j) ...
-                ", Ch 2" TrialKey.Ch2(j) ]); % TrialKey.Ch2_1(j) ", Ch3" TrialKey.Ch3(j)
+                ", Ch1" TrialKey.Ch1(j) ", Ch 2" TrialKey.Ch2(j)  ", Ch3" TrialKey.Ch3(j) TrialKey.Ch3_1(j)]); % TrialKey.Ch2_1(j) ", Ch3" TrialKey.Ch3(j)
 
             cd(file_path)
             TTS_output = readtable(current_csv_file); %, 'VariableNamingRule','preserve');
@@ -47,25 +47,25 @@ for i = 1:num_csv_files
             figure; 
             sgtitle(Trial_name)
             subplot(2,1,1)
-            plot(TTS_output.ms(1:end-2), TTS_output.TiltFeedback(1:end-2));
+            plot(TTS_output.ms(1:end-2)/1000, TTS_output.TiltFeedback(1:end-2));
             hold on;
-            plot(TTS_output.ms(1:end-2), TTS_output.GVS1MV(1:end-2));
-            % plot(TTS_output.ms(1:end-2), TTS_output.GVS2MV(1:end-2));
-            plot(TTS_output.ms(1:end-2), TTS_output.GVS3MV(1:end-2));
-            legend ("Tilt Angle", "Ch 2",  "Ch3")
+            plot(TTS_output.ms(1:end-2)/1000, TTS_output.GVS1MV(1:end-2));
+            plot(TTS_output.ms(1:end-2)/1000, TTS_output.GVS2MV(1:end-2));
+            plot(TTS_output.ms(1:end-2)/1000, TTS_output.GVS3MV(1:end-2));
+            legend ("Tilt Angle", "Ch 3",  "Ch2", "Ch1")
             xlabel("time (s)");
             ylabel("Angle (deg) and GVS (mV) ")
              
             subplot(2,1,2)
-            plot(TTS_output.ms(1:end-2), TTS_output.AngVel(1:end-2)); hold on;
-            plot(TTS_output.ms(1:end-2), TTS_output.GVS1MV(1:end-2));
-            % plot(TTS_output.ms(1:end-2), TTS_output.GVS2MV(1:end-2));
-            plot(TTS_output.ms(1:end-2), TTS_output.GVS3MV(1:end-2));
-            legend ("Tilt Velocity", "Ch 2",  "Ch3")
+            plot(TTS_output.ms(1:end-2)/1000, TTS_output.AngVel(1:end-2)); hold on;
+            plot(TTS_output.ms(1:end-2)/1000, TTS_output.GVS1MV(1:end-2));
+            plot(TTS_output.ms(1:end-2)/1000, TTS_output.GVS2MV(1:end-2));
+            plot(TTS_output.ms(1:end-2)/1000, TTS_output.GVS3MV(1:end-2));
+            legend ("Tilt Velocity", "Ch 3",  "Ch2", "Ch1")
             xlabel("time (s)");
             ylabel("Velocity (deg/s) and GVS (mV) ")
 
-            disp("press any key to continue")
+            % disp("press any key to continue")
             % pause
         else
             continue
