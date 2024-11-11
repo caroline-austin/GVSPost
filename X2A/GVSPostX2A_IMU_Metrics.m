63106070c3d12fd6d0e0d36e2370a7592f610433
caroline-austin <caroline.austin@colorado.edu>
2024-11-10 22:49:43 -0700

    Update GVSPostX2A_IMU_Metrics.m
    metrics to analyze amplitude and frequency of sway in the imu data. +bandaid fix for correcting data that crosses +/-180 mark

--- X2A/GVSPostX2A_IMU_Metrics.m
+++ X2A/GVSPostX2A_IMU_Metrics.m
@@ -17,7 +17,8 @@ subnum = [2001:2010];  % Subject List 2001:2010 2001:2010
 numsub = length(subnum);
 subskip = [2001 2008 2010];  %DNF'd subjects
 fs =100; % sampling freq of 100Hz
-
+profile_freq = [ 0 0 0.25 0.5 1];
+freq_interest = [ 0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.5 0.6 0.75 0.8 0.9 1 1.1 1.2 1.25];
 %% load data 
 for sub = 1:numsub
     subject = subnum(sub);
@@ -56,6 +57,7 @@ for sub = 1:numsub
 
         for current = 1:l
             for profile =1:w
+                % figure; 
                 for config = 1:h
                     if isempty(all_imu_data.(['A', subject_str]){current,profile,config})
                         continue
@@ -78,20 +80,47 @@ for sub = 1:numsub
                          trial_time_e = [trial_time; NaN(2*buffer,wid)];
                     end
                        
-                    neg_loc=find(trial_eulers < -100); % needs to be -70 for 2004 but -100 for every other sub to make roll and pitch look good
+                    if subject == 2004
+                        neg_loc=find(trial_eulers < -70); % needs to be -70 for 2004 but -100 for every other sub to make roll and pitch look good
                     trial_eulers(neg_loc) = trial_eulers(neg_loc)+360;
+                    else
+                        neg_loc=find(trial_eulers < -100); % needs to be -70 for 2004 but -100 for every other sub to make roll and pitch look good
+                        trial_eulers(neg_loc) = trial_eulers(neg_loc)+360;
+                    end
                     
                     bias = mean(trial_eulers);
                     trial_eulers = trial_eulers - bias; 
                     all_ang.(['A', subject_str]){current,profile,config} =  trial_eulers;
 
-                    if profile == 4 && config ==1
-                        y = trial_eulers(:,3);
-                        x = trial_time_e;
-                        mdl = fittype('a*sin(b*x+c)','indep','x');
-                        fittedmdl2 = fit(x,y,mdl,'start',[rand(),(pi),rand()]);
-                        figure; plot(x,y); hold on; plot(fittedmdl2);
-                    end
+                    roll_ang = trial_eulers(:,3);
+
+                    [power_interest{current, profile, config}(sub,:),~] = periodogram(roll_ang,[],freq_interest,fs);
+                    [med_freq{current, profile, config}(sub,:), med_power{current, profile, config}(sub,:)]=medfreq(roll_ang,fs);
+                    [mean_freq{current, profile, config}(sub,:), mean_power{current, profile, config}(sub,:)]=meanfreq(roll_ang,fs);
+
+
+                    mdl = fittype('a*sin(b*x+c)','indep','x');
+                    fittedmdl1 = fit(trial_time_e,roll_ang,mdl,'start',[rand(),profile_freq(profile)*pi(),rand()]);
+                    y_model = fittedmdl1(trial_time_e);
+                  
+                    phase_shift{current, profile, config}(sub,:) = fittedmdl1.c;
+                    fit_freq{current, profile, config}(sub,:) = fittedmdl1.b;
+                    fit_amp{current, profile, config}(sub,:) = fittedmdl1.a;
+                    
+                    [val, loc]=findpeaks(abs(y_model));
+                    amps = abs(diff(roll_ang (loc)))/2;
+                    mean_amp{current, profile, config}(sub,:) = mean(amps);
+                    med_amp{current, profile, config}(sub,:) = median(amps);
+
+                    % %for visual checking
+                    % subplot(3,1,config)
+                    % plot(trial_time_e,roll_ang);
+                    % hold on; plot(fittedmdl1);
+                    % hold on;plot(trial_time_e(loc),roll_ang(loc))
+                    % title(strjoin([subject_str "Profile:" Label.Config(config) Label.Profile(profile)  Label.CurrentAmp(current) " mA"]));
+
+
+
                     
                     % all_vel.(['A', subject_str]){current,profile,config}(:,1) = ...
                     %     cumtrapz(trial_time,trial_accel(:,1));
@@ -140,7 +169,9 @@ end
     cd([file_path]); %move to directory where file will be saved
     %add all variables that we want to save to a list must include space
     %between variable names 
-    vars_2_save =  ['Label all_imu_data rms_save all_imu_data  all_ang all_time ' ];% ...
+    vars_2_save =  ['Label all_imu_data rms_save all_imu_data  all_ang all_time ' ...
+        'freq_interest power_interest med_freq med_power mean_freq mean_power ' ...
+        'phase_shift fit_freq fit_amp mean_amp med_amp'];% ...
         % ' EndImpedance StartImpedance MaxCurrent MinCurrent all_pos all_vel']; 
     eval(['  save ' ['Allimu.mat '] vars_2_save ' vars_2_save']); %save file     
     cd(code_path) %return to code directory
