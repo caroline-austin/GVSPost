cfee682cf34c240fa9d3dc090f2fb1eed9422b83
caroline-austin <caroline.austin@colorado.edu>
2024-11-18 10:44:34 -0700

    Update GVSPostX2A_IMU_Plots.m
    added ability to plot subject symbols over top of the box plots

--- X2A/GVSPostX2A_IMU_Plots.m
+++ X2A/GVSPostX2A_IMU_Plots.m
@@ -13,6 +13,8 @@ subnum = [2001:2010];  % Subject List 2001:2010 2001:2010
 numsub = length(subnum);
 subskip = [2001 2004 2008 2010];  %DNF'd subjects
 
+
+
 %% plot info
  plots = [' 1 '];
 
@@ -44,6 +46,11 @@ Profiles = ["DC Right/Front"; "DC Left/Back"; "Sin 0.25Hz"; "Sin 0.5Hz"; "Sin 1H
 Profiles_safe = ["DCRight-Front"; "DCLeft-Back"; "Sin0_25Hz"; "Sin0_5Hz"; "Sin1Hz"];
 num_profiles = length(Profiles);
 
+sub_symbols = ["kpentagram";"k<";"khexagram";"k>"; "kdiamond";"kv";"ko";"k+"; "k*"; "kx"; "ksquare"; "k^";"k*";"khexagram";"kdiamond";];
+yoffset = [0.1;0.1;0.1;0.1;0.1;-0.1;-0.1;-0.1;-0.1;-0.1;0]; 
+yoffset2 = [0.05; -0.05;0.05;-0.05;0.05;-0.05]; 
+xoffset1 = [-100;-80;-60;-40;-20;0;20;40;60;80;100]; 
+xoffset2 = [-0.25;-0.2;-0.15; -0.15; -0.1;-0.05;0;0.05;0.1;0.15;0.2;0.25;0.1;-0.1;0.025]; 
 
 
 %% load data 
@@ -61,7 +68,7 @@ imu_dir = ['x' 'y' 'z' "roll" "pitch" "yaw" "yaw" "pitch" "roll"];
 
 if contains(plots,' 1 ')
 %% plot 1
-data_plot = single_metric_plot(subnum,subskip,imu_dir(4:6), Config , [1:3], Current_amp',[1:9], Profiles_safe, [4], rms_save, "Current Amplitude (mA)", "RMS (deg)");
+data_plot = single_metric_plot(subnum,subskip,imu_dir(4:6), Config , [1:3], Current_amp',[1:9], Profiles_safe, [4], rms_save, "Current Amplitude (mA)", "RMS (deg)", sub_symbols, xoffset2);
 
 
 %% plot 1
@@ -369,7 +376,7 @@ for sub = 1:numsub
 end
 
 
-function data_plot = single_metric_plot(subnum,subskip,figure_var, subplot_var,subplot_indices, trial_var, trial_indices, extra_var, extra_var_indices, data, x_label, y_label)
+function data_plot = single_metric_plot(subnum,subskip,figure_var, subplot_var,subplot_indices, trial_var, trial_indices, extra_var, extra_var_indices, data, x_label, y_label,sub_symbols, xoffset)
 
 numsub = length(subnum);
 num_figure_var = length(figure_var);
@@ -405,6 +412,18 @@ color_grad = turbo(num_trial_var);
             xticks(trial_indices);
             xticklabels(trial_var(trial_indices));
 
+            for sub = 1:numsub
+                subject = subnum(sub);
+                subject_str = num2str(subject);
+                 if ismember(subject,subskip) == 1
+                   continue
+                 end
+                for trial = 1:num_trial_var
+                    
+                    plot(trial+xoffset(sub), data_plot.(subplot_var(subplot)).(figure_var(figure_index))(sub,trial),sub_symbols(sub),'MarkerSize',15,"LineWidth", 1.5);
+                    hold on;
+                end
+            end
 
             if subplot ==2
                 ylabel(y_label)
