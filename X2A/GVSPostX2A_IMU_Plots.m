cb2a1b0b5085961648fc375be142004c325e9d7e
caroline-austin <caroline.austin@colorado.edu>
2024-11-18 09:17:30 -0700

    Update GVSPostX2A_IMU_Plots.m
    Added plot of mean freq and plot of power at 0.5

--- X2A/GVSPostX2A_IMU_Plots.m
+++ X2A/GVSPostX2A_IMU_Plots.m
@@ -129,7 +129,146 @@ sgtitle ('Yaw direction RMS of 0.5Hz Profiles ')
 end
 
 if contains(plots,'2 ')
-%% plot 2 - plot angle over time
+%% plot 2
+f1 = figure();
+tiledlayout(3,1, 'Padding', 'none', 'TileSpacing', 'compact'); 
+sgtitle ('Roll direction Mean Sway Freq of 0.5Hz Profiles ')
+% f2 = figure();
+% tiledlayout(3,1, 'Padding', 'none', 'TileSpacing', 'compact'); 
+% sgtitle ('Pitch direction RMS of 0.5Hz Profiles ')
+% f3 = figure();
+% tiledlayout(3,1, 'Padding', 'none', 'TileSpacing', 'compact'); 
+% sgtitle ('Yaw direction RMS of 0.5Hz Profiles ')
+% organize data into plotting variable
+    for j = 1:num_config
+        for i = 1:num_current
+            mean_freq_plot.(Config(j)).(imu_dir(4))(:,i) = mean_freq{i,4,j}(:,1);
+            % mean_freq_plot.(Config(j)).(imu_dir(5))(:,i) = mean_freq{i,4,j}(:,2);
+            % mean_freq_plot.(Config(j)).(imu_dir(6))(:,i) = mean_freq{i,4,j}(:,3);
+
+        end
+        figure(f1)
+        nexttile
+        boxplot(mean_freq_plot.(Config(j)).(imu_dir(4)));
+        hold on;
+        title(Config(j));
+        xticks([1,2,3,4,5,6,7,8,9]);
+        xticklabels(["0.1" ,"0.5" ,"1" ,"1.5" ,"2", "2.5" ,"3" ,"3.5" ,"4"]);
+        if j ==2
+            ylabel("RMS (deg/s)")
+        elseif j == 3
+        xlabel("Current Amplitude (mA)")
+        end
+        ylim([0 1.75]);
+        grid minor
+        
+
+        % figure(f2)
+        % nexttile
+        % boxplot(mean_freq_plot.(Config(j)).(imu_dir(5)));
+        % hold on;
+        % title(Config(j));
+        % xticks([1,2,3,4,5,6,7,8,9]);
+        % xticklabels(["0.1" ,"0.5" ,"1" ,"1.5" ,"2", "2.5" ,"3" ,"3.5" ,"4"]);
+        % if j ==2
+        %     ylabel("RMS (deg/s)")
+        % elseif j == 3
+        % xlabel("Current Amplitude (mA)")
+        % end
+        % % ylim([0 0.31]);
+        % grid minor
+        % 
+        % figure(f3)
+        % nexttile
+        % boxplot(mean_freq_plot.(Config(j)).(imu_dir(6)));
+        % hold on;
+        % title(Config(j));
+        % xticks([1,2,3,4,5,6,7,8,9]);
+        % xticklabels(["0.1" ,"0.5" ,"1" ,"1.5" ,"2", "2.5" ,"3" ,"3.5" ,"4"]);
+        % if j ==2
+        %     ylabel("RMS (deg/s)")
+        % elseif j == 3
+        % xlabel("Current Amplitude (mA)")
+        % end
+        % % ylim([0 0.25]);
+        % grid minor
+
+    end
+
+end
+
+if contains(plots,'3 ')
+%% plot 3
+f1 = figure();
+tiledlayout(3,1, 'Padding', 'none', 'TileSpacing', 'compact'); 
+sgtitle ('Roll direction Freq Power of 0.5Hz Profiles ')
+f2 = figure();
+tiledlayout(3,1, 'Padding', 'none', 'TileSpacing', 'compact'); 
+sgtitle ('Pitch direction Freq Power of 0.5Hz Profiles ')
+% f3 = figure();
+% tiledlayout(3,1, 'Padding', 'none', 'TileSpacing', 'compact'); 
+% sgtitle ('Yaw direction RMS of 0.5Hz Profiles ')
+% organize data into plotting variable
+    for j = 1:num_config
+        for i = 1:num_current
+            mean_freq_plot.(Config(j)).(imu_dir(4))(:,i) = power_interest_roll{i,4,j}(:,10);
+            mean_freq_plot.(Config(j)).(imu_dir(5))(:,i) = power_interest_pitch{i,4,j}(:,10);
+            % mean_freq_plot.(Config(j)).(imu_dir(6))(:,i) = mean_freq{i,4,j}(:,3);
+
+        end
+        figure(f1)
+        nexttile
+        boxplot(mean_freq_plot.(Config(j)).(imu_dir(4)));
+        hold on;
+        title(Config(j));
+        xticks([1,2,3,4,5,6,7,8,9]);
+        xticklabels(["0.1" ,"0.5" ,"1" ,"1.5" ,"2", "2.5" ,"3" ,"3.5" ,"4"]);
+        if j ==2
+            ylabel("Power (?)")
+        elseif j == 3
+        xlabel("Current Amplitude (mA)")
+        end
+        ylim([0 45]);
+        grid minor
+        
+
+        figure(f2)
+        nexttile
+        boxplot(mean_freq_plot.(Config(j)).(imu_dir(5)));
+        hold on;
+        title(Config(j));
+        xticks([1,2,3,4,5,6,7,8,9]);
+        xticklabels(["0.1" ,"0.5" ,"1" ,"1.5" ,"2", "2.5" ,"3" ,"3.5" ,"4"]);
+        if j ==2
+            ylabel("Power (?)")
+        elseif j == 3
+        xlabel("Current Amplitude (mA)")
+        end
+        ylim([0 15]);
+        grid minor
+        % 
+        % figure(f3)
+        % nexttile
+        % boxplot(mean_freq_plot.(Config(j)).(imu_dir(6)));
+        % hold on;
+        % title(Config(j));
+        % xticks([1,2,3,4,5,6,7,8,9]);
+        % xticklabels(["0.1" ,"0.5" ,"1" ,"1.5" ,"2", "2.5" ,"3" ,"3.5" ,"4"]);
+        % if j ==2
+        %     ylabel("RMS (deg/s)")
+        % elseif j == 3
+        % xlabel("Current Amplitude (mA)")
+        % end
+        % % ylim([0 0.25]);
+        % grid minor
+
+    end
+
+end
+
+
+if contains(plots,'4 ')
+%% plot 3 - plot angle over time
 data_plot = time_series_plot(subnum,subskip,imu_dir(7:9), Config , [1:3], Current_amp',[2:9], Profiles_safe, [4], all_ang, all_time, "Angle (deg)");
 disp(" press any key to close all")
 pause;
