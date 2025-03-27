68862a6626712913e8c83d3244cbd998cbeb536a
caroline-austin <caroline.austin@colorado.edu>
2025-03-27 16:02:53 -0600

    Updated angle calcs
    changed angle calculations to match X2A also there was an issue with how the data was being gravity aligned

--- X2B/X2B_IMUProcessing.m
+++ X2B/X2B_IMUProcessing.m
@@ -21,6 +21,8 @@ numsub = length(subnum);
 subskip = [40005 40006];  %DNF'd subjects or subjects that didn't complete this part
 
 sensorpositionplot = 0;
+fs = 30;
+dt = 1/fs;
 
 %%
 total_results = zeros(3,2);
@@ -63,10 +65,13 @@ for sub = 1:numsub
         load(IMU_files{file});
         x_tick_label{file_count,sub} = IMU_files{file}(13:16);
 
+        trial =  str2num(IMU_files{file}(end-5:end-4));
+
         Euler = imu_data(2:end,1:3); % exclude first line of 0's
         % not sure why, but order needs to be rotated like this- maybe so that the
         % axis that is most aligned with what we want to be z is the third?
-        acc = imu_data(2:end,[6,5,4]); 
+        % acc = imu_data(2:end,[6,5,4]); 
+        acc = imu_data(2:end,4:6); 
         gyro = pi/180*imu_data(2:end,7:9); % convert to rad/s
 
 
@@ -89,19 +94,58 @@ for sub = 1:numsub
             freq_SpHzmag(file_count,sub) = NaN;
             continue;
         end
-
+%%
         cd(code_path); cd .. ;
-        [acc_aligned, gyro_aligned, yaw, pitch, roll] = GravityAligned(acc, gyro,sensorpositionplot);
+        [acc_aligned, gyro_aligned, ~,~,~] = GravityAligned(acc(:,[2,3,1]), gyro(:,[2,3,1]),sensorpositionplot,fs);
         %[Xlimit,Ylimit] = PlotScale(imu_data,time);
         cutacc = acc_aligned(time_cut,:);
         cutgyro = gyro_aligned(time_cut,:);
-        cutyaw = yaw(time_cut);
-        cutroll = roll(time_cut);
-        cutpitch = pitch(time_cut);
+        % cutyaw = yaw(time_cut);
+        % cutroll = roll(time_cut);
+        % cutpitch = pitch(time_cut);
 
+        %% calculate the roll and pitch angles
+        % Initialize variables
+        roll = zeros(size(time));    % Roll angle
+        pitch = zeros(size(time));   % Pitch angle
+        yaw = zeros(size(time));     % Yaw angle (this example doesn't calculate yaw)
+        gyro_angle = zeros(3, length(time)); % Gyro angle
+        acc_angle = zeros(3, length(time)); % Accelerometer angle
+        alpha = .90; % Complementary filter constant (tune this value)
 
-        data_type = ["EulerX","EulerY","EulerZ","accX","accY","accZ","gyroX","gyroY","gyroZ"];
-        direction = ["Z", "Y", "X"];
+        % Initial angles from accelerometer (first estimate)
+        acc_angle(1, 1) = atan2(acc_aligned(2,1), acc_aligned(3,1)); % Roll
+        acc_angle(2, 1) = atan2(-acc_aligned(1,1), sqrt(acc_aligned(2,1)^2 + acc_aligned(3,1)^2)); % Pitch
+        
+        % Process each time step
+        for i = 2:length(time)
+            % Gyroscope integration to get the angular velocity
+            gyro_angle(:, i) = gyro_angle(:, i-1) + gyro_aligned(i-1,: )' * dt; % Integrate gyro data to get angle
+            
+            % Accelerometer-based angle estimation (assuming accelerometer gives tilt)
+            acc_angle(1, i) = atan2(acc_aligned(i,2), acc_aligned(i,3)); % Roll from accelerometer
+            acc_angle(2, i) = atan2(-acc_aligned(i,1), sqrt(acc_aligned(i,2)^2 + acc_aligned(i,3)^2)); % Pitch from accelerometer
+        
+            % Apply complementary filter to combine accelerometer and
+            % gyroscope data- accelerometer data corrects for the gyroscope
+            % drift
+            roll(i) = alpha * (roll(i-1) + gyro_aligned( i-1,1) * dt) + (1 - alpha) * acc_angle(1,i); % Roll estimate
+            pitch(i) = alpha * (pitch(i-1) + gyro_aligned(i-1,2) * dt) + (1 - alpha) * acc_angle(2,i); % Pitch estimate
+        
+            % If you want to calculate yaw, you would need magnetometer
+            % data or use more advanced sensor fusion to correct for the
+            % drift
+            yaw(i) = yaw(i-1) + gyro_aligned(i-1,3 ) * dt; % Gyroscope yaw estimate (if available)
+        end
+
+        tilt_angles= [roll; pitch; yaw]';
+        
+        imu_angles{trial} = tilt_angles; 
+        Label.imu_angles = ["roll" "pitch" "yaw"];
+        
+
+        % data_type = ["EulerX","EulerY","EulerZ","accX","accY","accZ","gyroX","gyroY","gyroZ"];
+        % direction = ["Z", "Y", "X"];
 
         % magnitude of gyro and acc 
         mag_acc = sqrt((cutacc(:,2).^2) + (cutacc(:,3).^2));
@@ -141,7 +185,20 @@ for sub = 1:numsub
     
         end
 
+
+
     end
+
+    %% Save file
+    cd([file_path, '/' , subject_str]); %move to directory where file will be saved
+    %add all variables that we want to save to a list must include space
+    %between variable names 
+    vars_2_save =  ['Label  imu_data imu_angles acc_aligned gyro_aligned ' ...
+        '']; 
+    eval(['  save ' ['A', subject_str,'imu.mat '] vars_2_save ' vars_2_save fs']); %save file     
+    cd(code_path) %return to code directory
+    %clear saved variables to prevent them from affecting next subjects' data
+    eval (['clear ' vars_2_save]) 
 
 end
 
