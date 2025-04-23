093e8b0d4951f8a7a549116fc7726738f85ce322
caroline-austin <caroline.austin@colorado.edu>
2025-04-23 17:13:48 -0600

    Update X3BGetFiles.m
    Data read in pipeline modifications. Pulling from the excel file and sorting through romberg data should be complete (may still need debugging modifications)

--- X3B/X3BGetFiles.m
+++ X3B/X3BGetFiles.m
@@ -1,4 +1,4 @@
-%% Script 1 for Manual Control Nulling Testing
+%% Script 1 for X3B Fuctional Mobility and Manual Control Nulling Testing
 % This script reads in information from both the master spreadsheet for the
 % experiment and the individual TTS and GVS? files. It compares the
 % filenames to the expected name info specified in the master spreadsheet
@@ -9,7 +9,7 @@
 close all; clear; clc; 
 
 %% setup
-subnum = [ 3004:3004];  % Subject List 1011:1022
+subnum = [ 3091:3091];  % Subject List 1011:1022
 numsub = length(subnum);
 subskip = [1013 40005 40006];  %DNF'd subjects or subjects that didn't complete this part 
 
@@ -39,15 +39,163 @@ for sub = 1:numsub
 % Even though the document contains info from all subjects this pull from
 % only the current subject's sheet
     cd(file_path);
-    Trial_Info = readcell('PilotTestTrials.xlsx','Sheet', ... % ManualControlTrials.xlsx
-        ['S' subject_str],'Range','A2:H35'); % may need to increase beyond 35, but make sure those rows have zeros
-    Trial_Num = cell2mat(Trial_Info(:,1));
-    Label.DataColumns = readcell('PilotTestTrials.xlsx','Sheet',... 
-        ['S' subject_str], 'Range','A1:H1');
-    
+
+    % Tandem walk info
+    Trial_Info_TDM = readcell('FunctionalMobilityTesting.xlsx','Sheet', ... % ManualControlTrials.xlsx
+        ['S' subject_str],'Range','K28:S41'); % may need to increase beyond 35, but make sure those rows have zeros
+    Trial_Num_TDM = cell2mat(Trial_Info_TDM(:,1));
+    Label.DataColumnsTDM = readcell('FunctionalMobilityTesting.xlsx','Sheet',... 
+        ['S' subject_str], 'Range','K27:S27');
+
+    % Fuctional Mobility info
+    Trial_Info_FMT = readcell('FunctionalMobilityTesting.xlsx','Sheet', ... % ManualControlTrials.xlsx
+        ['S' subject_str],'Range','A28:I41'); % may need to increase beyond 35, but make sure those rows have zeros
+    Trial_Num_FMT = cell2mat(Trial_Info_FMT(:,1));
+    Label.DataColumnsFMT = readcell('FunctionalMobilityTesting.xlsx','Sheet',... 
+        ['S' subject_str], 'Range','A27:I27');
+
+    % Romberg info
+    Trial_Info_ROM = readcell('FunctionalMobilityTesting.xlsx','Sheet', ... % ManualControlTrials.xlsx
+        ['S' subject_str],'Range','A3:L23'); % may need to increase beyond 35, but make sure those rows have zeros
+    Trial_Num_ROM = cell2mat(Trial_Info_ROM(:,1));
+    Label.DataColumnsROM = readcell('FunctionalMobilityTesting.xlsx','Sheet',... 
+        ['S' subject_str], 'Range','A2:L2');
+
+    % TTS info
+    Trial_Info_TTS = readcell('FunctionalMobilityTesting.xlsx','Sheet', ... % ManualControlTrials.xlsx
+        ['S' subject_str],'Range','N3:T19'); % may need to increase beyond 35, but make sure those rows have zeros
+    Trial_Num_TTS = cell2mat(Trial_Info_TTS(:,1));
+    Label.DataColumnsTTS = readcell('FunctionalMobilityTesting.xlsx','Sheet',... 
+        ['S' subject_str], 'Range','N2:T2');
+%%
+ % get info on Romberg files 
+    cd([code_path '/..']);
+    ROM_path = [subject_path '\Romberg'] ;
+    ROM_GIST_path = [subject_path '\Romberg\GIST'] ;
+    ROM_Wii_path = [subject_path '\Romberg\Wii'] ;
+    [ROM_GIST_filenames]=file_path_info2(code_path,ROM_GIST_path ); % get files from file folder
+    cd([code_path '/..']);
+    [ROM_Wii_filenames]=file_path_info2(code_path,ROM_Wii_path ); % get files from file folder
+    num_ROM_GIST_files = length(ROM_GIST_filenames);
+    num_ROM_Wii_files = length(ROM_Wii_filenames);
+
+    % for each romberg GIST file (trial) grab associated Wii Force Plate
+    % data and condition info for naming then save as a matlab file. 
+    Wii_file_index = 0;
+    Wii_trial_index = 1;
+    num_Wii_trials = 0;
+    ROM_trial_index = 0;
+    for i = 1:num_ROM_GIST_files
+        current_file = char(ROM_GIST_filenames(i));
+       if ~ contains(current_file, 'csv') 
+           continue
+       end
+       ROM_trial_index = ROM_trial_index +1;
+        cd(ROM_GIST_path);
+        ROM_GIST_data = readtable(current_file); 
+        cd(code_path);
+        
+
+        if Wii_trial_index > num_Wii_trials
+            Wii_file_index = Wii_file_index +1;
+            cd(ROM_Wii_path);
+            ROM_Wii_data = readtable(char(ROM_Wii_filenames(Wii_file_index)));
+            cd(code_path);
+            
+            for j = 2:length(ROM_Wii_data.Var1)
+                timesteps(j) = ROM_Wii_data.Var1(j) - ROM_Wii_data.Var1(j-1);    
+            end
+
+            trial_start_loc = [1 find(timesteps > 100)];
+            num_Wii_trials = length(trial_start_loc);
+            trial_start_loc = [trial_start_loc length(ROM_Wii_data.Var1)];
+            Wii_trial_index = 1;
+
+        end
+
+        ROM_Wii_trial = ROM_Wii_data{trial_start_loc(Wii_trial_index):(trial_start_loc(Wii_trial_index+1)-1) ,:};
+        
+
+        % get Trial Info from excel sheet
+        if Wii_file_index == 1
+            ROM_trial_row_index = Wii_trial_index;
+            ROM_trial_col_index = 7;
+            
+        elseif num_ROM_Wii_files >2 % for blocking 
+            ROM_trial_row_index = 6*(Wii_trial_index-1)+ 1+ rem((Wii_trial_index+1),2);
+            ROM_trial_col_index = 7+ rem((Wii_trial_index+1),2);
+
+        else % for the single giant file
+            ROM_trial_row_index = 6+(ceil(Wii_trial_index/2));%- rem((Wii_trial_index+1),2);
+            ROM_trial_col_index = 7+ rem((Wii_trial_index+1),2);
+            
+        end
+
+        ROM_trial_info = Trial_Info_ROM(ROM_trial_row_index, :) ;
+        ROM_pass = ROM_trial_info{ROM_trial_col_index};
+        
+        ROM_trial_name = 'ROM';
+        % put together trial_name
+        switch ROM_trial_info{4}
+            case {'open' , 'Open'}
+                ROM_trial_name = [ROM_trial_name '_EO'];
+
+            case {'closed' , 'Closed'}
+                ROM_trial_name = [ROM_trial_name '_EC'];
+        end
+
+        switch ROM_trial_info{6}
+            case 'hard'
+                ROM_trial_name = [ROM_trial_name '_HS'];
+            case 'foam'
+                ROM_trial_name = [ROM_trial_name '_FS'];
+        end
+
+        switch ROM_trial_info{5}
+            case 'none'
+                ROM_trial_name = [ROM_trial_name '_NHT'];
+
+            case {'pitch', 'Pitch'}
+                ROM_trial_name = [ROM_trial_name '_PHT'];
+
+            case {'roll', 'Roll'}
+                ROM_trial_name = [ROM_trial_name '_RHT'];
+        end
+
+
+
+        switch ROM_trial_info{3}
+            case 'Sham'
+                ROM_trial_name = [ROM_trial_name '_SHAMGVS'];
+
+            case 'DC Offset'
+                ROM_trial_name = [ROM_trial_name '_DCSDGVS'];
+
+            case 'DC Wave'
+                ROM_trial_name = [ROM_trial_name '_DCONGVS'];
+        end
+
+        if ROM_trial_index <10
+            ROM_trial_name = [ROM_trial_name '_0' num2str(ROM_trial_index)];
+        else
+            ROM_trial_name = [ROM_trial_name '_' num2str(ROM_trial_index)];
+        end
+
+        cd(ROM_path);
+       vars_2_save = ['ROM_GIST_data ROM_Wii_trial ROM_trial_info' ...
+           ];
+       eval(['  save ' [char(ROM_trial_name), '.mat '] vars_2_save ' vars_2_save']);      
+       cd(code_path)
+       eval (['clear ' vars_2_save])
+
+        Wii_trial_index = Wii_trial_index+1;
+    end
+
+
     % get info on the csv files (TTS output files) for the current subject
     cd([code_path '/..']);
-    [subject_filenames]=file_path_info2(code_path,subject_path ); % get files from file folder
+    TTS_path = [subject_path '\TTS'] ;
+    [subject_filenames]=file_path_info2(code_path,TTS_path ); % get files from file folder
     num_sub_files = length(subject_filenames);
     cd([code_path '/..']);
     csv_filenames = get_filetype(subject_filenames, 'csv');
@@ -68,7 +216,7 @@ for sub = 1:numsub
        trial_loc = find(current_csv_file == ' ', 1,"last"); % there should be a space right before the trial number
        trial_number = current_csv_file(trial_loc+1:trial_loc+2);
        %find the matching trial number from the master spreadsheet
-       row = find(Trial_Num == str2num(trial_number),1);
+       row = find(Trial_Num_TTS == str2num(trial_number),1);
        % if there is not a matching number in the master spreadsheet skip
        % (could be a practice trial or a trial that was thrown out)
        if isempty(row)
@@ -81,7 +229,7 @@ for sub = 1:numsub
        end
 
        % % figure out the filename of the GVS profile that was run
-       GVS_profile_name = char(strjoin([ string(Trial_Info(row,3))]));
+       GVS_profile_name = char(strjoin([ string(Trial_Info_TTS(row,3))]));
         
        % %figure out what the current was proportional to (7,7.5,8) and store
        % %in a string to use later for file naming
@@ -98,7 +246,7 @@ for sub = 1:numsub
        % 
        % figure out name of the TTS profile that was run (this is 
        % based on the master spread sheet, not the csv filename)
-       TTS_profile = char(string(Trial_Info(row,4)));%this might change if additional GVS columns are added
+       TTS_profile = char(string(Trial_Info_TTS(row,4)));%this might change if additional GVS columns are added
        TTS_profile_name = ['Esther_MC_', TTS_profile ];
 
        %check actual v. perscribed physical motion profile
